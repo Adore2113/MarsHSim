e9754d7b0563b484cd9d926f1276301ab30e7e22
Adore2113 <adore.codes@gmail.com>
2026-03-17 20:03:02 -0400

    fixed the buffer gas crontrol function to include considering the storage amounts and target gas amounts.

--- src/sim/engine.py
+++ src/sim/engine.py
@@ -143,33 +143,32 @@ def run_buffer_gas_control(state):
     total_pressure_kpa = mca(state)
 
     if total_pressure_kpa <= state.min_safe_pressure_kpa:
-        amount_to_add_kpa = state.target_pressure_kpa - total_pressure_kpa
+        pressure_needed_kpa = state.target_pressure_kpa - total_pressure_kpa
         
-        if state.n2_stored_kpa >= amount_to_add_kpa:    # emergency (pressure recover first)
-            state.n2_kpa += amount_to_add_kpa
-            state.n2_stored_kpa -= amount_to_add_kpa    # capped by avaliable supply
-        
+        if state.n2_stored_kpa > 0 and state.n2_stored_kpa >= pressure_needed_kpa:
+            state.n2_kpa += pressure_needed_kpa
+            state.n2_stored_kpa -= pressure_needed_kpa
         else:
             state.n2_kpa += state.n2_stored_kpa
             state.n2_stored_kpa = 0.0
 
-    elif total_pressure_kpa < state.target_pressure_kpa:    # pressure balance first, but capped so it doesn't over-add
-        amount_to_add_kpa = state.target_pressure_kpa - total_pressure_kpa
+    elif total_pressure_kpa < state.target_pressure_kpa:
+        pressure_needed_kpa = state.target_pressure_kpa - total_pressure_kpa
 
         if state.n2_kpa < state.target_n2_kpa:
-            n2_space_kpa = state.target_n2_kpa - state.n2_kpa
-            n2_to_add_kpa = min(amount_to_add_kpa, n2_space_kpa)
+            n2_room_left_kpa = state.target_n2_kpa - state.n2_kpa
+            n2_to_add_kpa = min(pressure_needed_kpa, n2_room_left_kpa, state.n2_stored_kpa)
             state.n2_kpa += n2_to_add_kpa
-            amount_to_add_kpa -= n2_to_add_kpa
+            state.n2_stored_kpa -= n2_to_add_kpa
+            pressure_needed_kpa -= n2_to_add_kpa
         
-        if amount_to_add_kpa > 0 and state.ar_kpa < state.target_ar_kpa:
-            ar_space_kpa = state.target_ar_kpa - state.ar_kpa
-            ar_to_add_kpa = min(amount_to_add_kpa, ar_space_kpa)
+        if pressure_needed_kpa > 0 and state.ar_kpa < state.target_ar_kpa:
+            ar_room_left_kpa = state.target_ar_kpa - state.ar_kpa
+            ar_to_add_kpa = min(pressure_needed_kpa, ar_room_left_kpa, state.ar_stored_kpa)
             state.ar_kpa += ar_to_add_kpa
-            amount_to_add_kpa -= ar_to_add_kpa
+            state.ar_stored_kpa -= ar_to_add_kpa
+            pressure_needed_kpa -= ar_to_add_kpa
     
-    return state
-
 
 # ----alerts ----
 def gas_alert(state):
