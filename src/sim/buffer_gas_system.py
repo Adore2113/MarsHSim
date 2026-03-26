741c7720056b59cf880c39adf29ddbabc492c9a5
Adore2113 <adore.codes@gmail.com>
2026-03-26 17:15:44 -0400

    broke one long function to seperate functions for readability, future handling and organization.

--- src/sim/buffer_gas_system.py
+++ src/sim/buffer_gas_system.py
@@ -1,8 +1,7 @@
 from dataclasses import replace
 from .state import Habitat_State
 
-# file for Major Constituant Analyzer and buffer gas control
-
+# file for Major Constituent Analyzer and buffer gas control
 
 # ----checking atmosphere gas levels---- 
 def mca(o2_kpa, co2_kpa, n2_kpa, ar_kpa): 
@@ -12,35 +11,31 @@ def mca(o2_kpa, co2_kpa, n2_kpa, ar_kpa):
 
 
 # ----controlling atmosphere gas levels----
-def run_buffer_gas_control(state, dt_min):
-    hours_per_step = dt_min / 60
-
+def buffer_gas_regen_kpa(state):
     n2_kpa = state.n2_kpa
     n2_stored_kpa = state.n2_stored_kpa
     ar_kpa = state.ar_kpa
     ar_stored_kpa = state.ar_stored_kpa
 
     total_buffer_gas_added_kpa = 0.0
+    total_pressure_kpa = mca(state.o2_kpa, state.co2_kpa, n2_kpa, ar_kpa)
 
-    buffer_gas_heat_per_kpa_kw = 1.5
-
-    buffer_gas_heat_added_kw = 0.0
-    buffer_gas_heat_added_kwh = 0.0
-
-    total_pressure_kpa = state.o2_kpa + state.co2_kpa + n2_kpa + ar_kpa
-
+    # ----emergency control----
     if total_pressure_kpa <= state.min_safe_pressure_kpa:
         pressure_needed_kpa = state.target_pressure_kpa - total_pressure_kpa
 
-        if n2_stored_kpa > 0 and n2_stored_kpa >= pressure_needed_kpa:
-           n2_kpa += pressure_needed_kpa
-           n2_stored_kpa -= pressure_needed_kpa
-           total_buffer_gas_added_kpa += pressure_needed_kpa
+        n2_to_add_kpa = min(pressure_needed_kpa, n2_stored_kpa)    # use nitrogen first
+        n2_kpa += n2_to_add_kpa
+        n2_stored_kpa -= n2_to_add_kpa
+        total_buffer_gas_added_kpa += n2_to_add_kpa
+        pressure_needed_kpa -= n2_to_add_kpa
 
-        else:
-            n2_kpa += n2_stored_kpa
-            total_buffer_gas_added_kpa += n2_stored_kpa
-            n2_stored_kpa = 0.0
+        if pressure_needed_kpa > 0:    # use argon next
+            ar_to_add_kpa = min(pressure_needed_kpa, ar_stored_kpa)
+            ar_kpa += ar_to_add_kpa
+            ar_stored_kpa -= ar_to_add_kpa
+            total_buffer_gas_added_kpa += ar_to_add_kpa
+            pressure_needed_kpa -= ar_to_add_kpa
 
     elif total_pressure_kpa < state.target_pressure_kpa:
         pressure_needed_kpa = state.target_pressure_kpa - total_pressure_kpa
@@ -52,7 +47,6 @@ def run_buffer_gas_control(state, dt_min):
             n2_kpa += n2_to_add_kpa
             n2_stored_kpa -= n2_to_add_kpa
             total_buffer_gas_added_kpa += n2_to_add_kpa
-            
             pressure_needed_kpa -= n2_to_add_kpa
 
         if pressure_needed_kpa > 0 and ar_kpa < state.target_ar_kpa:
@@ -62,21 +56,44 @@ def run_buffer_gas_control(state, dt_min):
             ar_kpa += ar_to_add_kpa
             ar_stored_kpa -= ar_to_add_kpa
             total_buffer_gas_added_kpa += ar_to_add_kpa
-            
             pressure_needed_kpa -= ar_to_add_kpa
+        
+    return n2_kpa, ar_kpa, n2_stored_kpa, ar_stored_kpa, total_buffer_gas_added_kpa    
+      
+
+def buffer_gas_power_and_heat(total_buffer_gas_added_kpa, dt_min):
+    hours_per_step = dt_min / 60
+
+    if total_buffer_gas_added_kpa > 0:
+        buffer_gas_heat_per_kpa_kw = 1.5
+        buffer_gas_heat_added_kw = total_buffer_gas_added_kpa * buffer_gas_heat_per_kpa_kw
+        buffer_gas_heat_added_kwh = buffer_gas_heat_added_kw * hours_per_step
+    
+        buffer_gas_power_used_kw = 0.4
+        buffer_gas_energy_used_kwh = buffer_gas_power_used_kw * hours_per_step
 
     else:
-        pass
+        buffer_gas_heat_added_kw = 0.0
+        buffer_gas_heat_added_kwh = 0.0
 
-    buffer_gas_heat_added_kw = total_buffer_gas_added_kpa * buffer_gas_heat_per_kpa_kw
-    buffer_gas_heat_added_kwh = buffer_gas_heat_added_kw * hours_per_step
+        buffer_gas_power_used_kw = 0.0
+        buffer_gas_energy_used_kwh = 0.0
     
+    return buffer_gas_heat_added_kw, buffer_gas_heat_added_kwh, buffer_gas_power_used_kw, buffer_gas_energy_used_kwh
+
+def run_buffer_gas_control(state, dt_min):
+    n2_kpa, ar_kpa, n2_stored_kpa, ar_stored_kpa, total_buffer_gas_added_kpa = buffer_gas_regen_kpa(state)
+    buffer_gas_heat_added_kw, buffer_gas_heat_added_kwh, buffer_gas_power_used_kw, buffer_gas_energy_used_kwh = buffer_gas_power_and_heat(total_buffer_gas_added_kpa, dt_min)
+
     return {
         "n2_kpa" : n2_kpa,
         "ar_kpa" : ar_kpa,
-        "n2_stored_kpa": n2_stored_kpa,
-        "ar_stored_kpa": ar_stored_kpa,
-        "buffer_gas_heat_added_kw": buffer_gas_heat_added_kw,
-        "buffer_gas_heat_added_kwh": buffer_gas_heat_added_kwh,
+        "n2_stored_kpa" : n2_stored_kpa,
+        "ar_stored_kpa" : ar_stored_kpa,
+        "total_buffer_gas_added_kpa" : total_buffer_gas_added_kpa,
+        "buffer_gas_heat_added_kw" : buffer_gas_heat_added_kw,
+        "buffer_gas_heat_added_kwh" : buffer_gas_heat_added_kwh,
+        "buffer_gas_power_used_kw" : buffer_gas_power_used_kw, 
+        "buffer_gas_energy_used_kwh" : buffer_gas_energy_used_kwh,
     }
 
