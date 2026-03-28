6e66a753650da89432f3e4852d42d83f83f03e26
Adore2113 <adore.codes@gmail.com>
2026-03-28 11:03:00 -0400

    moved crew metabolism function into it's own file.

--- src/sim/engine.py
+++ src/sim/engine.py
@@ -3,6 +3,7 @@ from .state import Habitat_State
 from .oxygen_system import run_oga
 from .buffer_gas_system import mca, run_buffer_gas_control
 from .co2_scrubber_system import run_co2_scrub
+from crew_metabolism import crew_metabolism
 
 # ----default timestep----
 default_dt_min = 5
@@ -36,30 +37,8 @@ def get_sol_time(mission_time_s):
 
 
 # ----crew metabolism per default timestep----
-def crew_metabolism(state, dt_min):
-    hours_per_step = dt_min / 60
-    # atmosphere gases
-    o2_drop_kpa = 0.00011 * state.crew_count    # =: 0.0033
-    co2_rise_kpa = 0.0000967 * state.crew_count    # = 0.0029
 
-    # tempurature
-    if state.crew_activity == "sleep":
-        crew_temp_rise_kw = 0.083 * state.crew_count    # = 2.49
-        crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
-
-    elif state.crew_activity == "exercise":   
-        crew_temp_rise_kw = 0.280 * state.crew_count    # = 9.0
-        crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
-    
-    elif state.crew_activity == "intense":
-        crew_temp_rise_kw = 0.350 * state.crew_count    # = 10.5
-        crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
-    
-    else: 
-        crew_temp_rise_kw = 0.120 * state.crew_count    # = 3.78
-        crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
-
-    return o2_drop_kpa, co2_rise_kpa, crew_temp_rise_kw, crew_temp_rise_kwh
+# moved to crew_metabolism.py
 
 
 def lights(state, dt_min):
