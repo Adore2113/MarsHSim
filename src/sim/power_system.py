5a10262c8aa4639978387629eef9d2a447577e54
Adore2113 <adore.codes@gmail.com>
2026-03-29 14:35:15 -0400

    added first function to file that returns total power and energy usage from co2, oga and lights.

--- src/sim/power_system.py
+++ src/sim/power_system.py
@@ -1,3 +1,12 @@
 from dataclasses import replace
 from .state import Habitat_State
 
+def power_usage_kw(outputs):
+    total_power_used_kw = 0.0
+    total_energy_used_kw = 0.0
+
+    total_power_used_kw = outputs["co2_scrubber_power_used_kw"] + outputs["oga_power_used_kw"] + outputs["light_power_kw"]
+    total_energy_used_kwh = outputs["co2_scrubber_energy_used_kwh"] + outputs["oga_energy_used_kwh"] + outputs["light_power_used_kwh"]
+  
+    return total_power_used_kw, total_energy_used_kwh
+ 
