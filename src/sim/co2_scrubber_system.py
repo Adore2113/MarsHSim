0879ef54c417637c44b79f07c81ea81a6b0120ab
Adore2113 <adore.codes@gmail.com>
2026-03-28 20:27:02 -0400

    properly imported crew_metabolism file and added power usage.

--- src/sim/co2_scrubber_system.py
+++ src/sim/co2_scrubber_system.py
@@ -1,6 +1,6 @@
 from dataclasses import replace
 from .state import Habitat_State
-from crew_metabolism import crew_metabolism
+from .crew_metabolism import crew_metabolism
 
 # file for co2 removal and amine bed functions
 
@@ -38,24 +38,36 @@ def co2_removed_and_storage_update(state, co2_after_crew_kpa, max_scrub_removal_
     return co2_after_scrub_kpa, co2_removed_kpa, new_co2_stored_kpa
 
 
-def co2_scrub_gas_power_and_heat(co2_removed_kpa, bed_online_count, dt_min):
+def co2_scrub_gas_power_and_heat(co2_removed_kpa, bed_online_count, next_time_s, dt_min):
     hours_per_step = dt_min / 60
 
-    co2_scrubber_heat_per_kpa_kw = 1200.0
-    co2_scrubber_heat_added_kw = co2_removed_kpa * co2_scrubber_heat_per_kpa_kw / 1000.0
+    if co2_removed_kpa > 0:
+        co2_scrubber_heat_per_kpa_kw = 1200.0
+        co2_scrubber_heat_added_kw = co2_removed_kpa * co2_scrubber_heat_per_kpa_kw / 1000.0
+        baseline_bed_heat_kw = bed_online_count * 0.4
+        
+        co2_scrubber_heat_added_kw = co2_scrubber_heat_added_kw + baseline_bed_heat_kw
+        co2_scrubber_heat_added_kwh = co2_scrubber_heat_added_kw * hours_per_step
+
+        baseline_bed_power_used_kw = bed_online_count * 0.6    # placeholder amount
+        co2_removal_power_per_kpa_kw = 1500.0    # placeholder amount
+        co2_removal_power_used_kw = co2_removed_kpa * co2_removal_power_per_kpa_kw / 1000.0
+
+        co2_scrubber_power_used_kw = baseline_bed_power_used_kw + co2_removal_power_used_kw
+        
+        if next_time_s % 3300 == 0 and next_time_s != 0:    # temporary power increase when beds switch
+            co2_scrubber_power_used_kw *= 1.10
+
+        co2_scrubber_energy_used_kwh = co2_scrubber_power_used_kw * hours_per_step
     
-    baseline_bed_heat_added_kw = bed_online_count * 0.4
-
-    co2_scrubber_heat_added_kw = co2_scrubber_heat_added_kw + baseline_bed_heat_added_kw
-    co2_scrubber_heat_added_kwh = co2_scrubber_heat_added_kw * hours_per_step
-
-    return co2_scrubber_heat_added_kw, co2_scrubber_heat_added_kwh #, co2_scrubber_power_used_kw, co2_scrubber_energy_used_kwh
+    return co2_scrubber_heat_added_kw, co2_scrubber_heat_added_kwh, co2_scrubber_power_used_kw, co2_scrubber_energy_used_kwh
 
 
 def run_co2_scrub(state, co2_after_crew_kpa, next_time_s, dt_min):
     max_scrub_removal_kpa, beds_online = co2_scrub_capacity_kpa(state, co2_after_crew_kpa, next_time_s)
     co2_after_scrub_kpa, co2_removed_kpa, new_co2_stored_kpa = co2_removed_and_storage_update(co2_removed_kpa, beds_online, dt_min)
     co2_scrubber_heat_kw, co2_scrubber_heat_kwh = co2_scrub_gas_power_and_heat(co2_removed_kpa, bed_online_count, dt_min)
+    co2_scrubber_heat_kw, co2_scrubber_heat_kwh, co2_scrubber_power_used_kw, co2_scrubber_energy_used_kwh = co2_scrub_gas_power_and_heat(co2_removed_kpa, beds_online, next_time_s, dt_min)
 
     return {
         "co2_after_scrub_kpa": co2_after_scrub_kpa,
@@ -63,6 +75,6 @@ def run_co2_scrub(state, co2_after_crew_kpa, next_time_s, dt_min):
         "new_co2_stored_kpa": new_co2_stored_kpa,
         "co2_scrubber_heat_kw": co2_scrubber_heat_kw,
         "co2_scrubber_heat_kwh": co2_scrubber_heat_kwh,
-        #"co2_scrubber_power_used_kw": co2_scrubber_power_used_kw,
-        #"co2_scrubber_energy_used_kwh": co2_scrubber_energy_used_kwh
+        "co2_scrubber_power_used_kw": co2_scrubber_power_used_kw,
+        "co2_scrubber_energy_used_kwh": co2_scrubber_energy_used_kwh
     }
