fc3719eaddd8d5eacc9f5dac93f0d9862fb1c6f3
Adore2113 <adore.codes@gmail.com>
2026-03-29 22:26:52 -0400

    started solar generation function.

--- src/sim/power_system.py
+++ src/sim/power_system.py
@@ -1,6 +1,16 @@
 from dataclasses import replace
 from .state import Habitat_State
 
+# state.battery_max_capacity_kwh
+# state.battery_stored_kwh 
+# state.solar_capacity_kw
+# state.solar_efficiency
+
+def solar_generation_kw(state):
+    ...
+
+
+
 def power_usage_kw(outputs):
     total_power_used_kw = 0.0
     total_energy_used_kw = 0.0
