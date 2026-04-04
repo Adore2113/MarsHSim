3fb6e74535241d879cd887dca3757576fc7c73f5
Adore2113 <adore.codes@gmail.com>
2026-04-04 15:23:26 -0400

    moved sol_time function to here.

--- src/sim/mars_time.py
+++ src/sim/mars_time.py
@@ -3,3 +3,21 @@ from .state import Habitat_State
 
 # file for handling all things time
 
+def sol_time(seconds):
+    total_sol_seconds = 88775    # one mars sol is 24h 39min 35sec
+    sol_seconds = seconds % total_sol_seconds
+    
+    hour_24 = sol_seconds // 3600
+    minutes = (sol_seconds % 3600) // 60     # 1h = 60min, 1min = 60 sec, 60*60 = 3600
+    
+    meridiem = "AM"
+    hour_12 = hour_24
+
+    if hour_24 >= 12:
+        meridiem = "PM"
+    if hour_24 > 12:
+        hour_12 = hour_24 - 12
+    if hour_24 == 0:
+        hour_12 = 12
+
+    return hour_12, minutes, meridiem
