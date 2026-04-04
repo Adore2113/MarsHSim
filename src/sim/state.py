6cf29efb7560d2d461b75e4c4859435577da5404
Adore2113 <adore.codes@gmail.com>
2026-04-04 16:12:50 -0400

    renamed 'how_much_daylight_kw_m2' to 'daylight_m2_kw'.

--- src/sim/state.py
+++ src/sim/state.py
@@ -81,11 +81,9 @@ class Habitat_State:
     battery_max_capacity_kwh: float
     battery_stored_kwh: float 
     solar_arrays: list
-    how_much_daylight_kw_m2: float
+    daylight_m2_kw: float
 
     # ----placeholders from here down----
-    how_much_daylight_kw_m2: float
-   
     relative_humidity: float
 
     # Water
