6905d718ff6cc19378bc263023b96f0816cfa237
Adore2113 <adore.codes@gmail.com>
2026-04-04 17:00:12 -0400

    removed redundant variables at the top of power_usage_kw function.

--- src/sim/power_system.py
+++ src/sim/power_system.py
@@ -36,9 +36,6 @@ def solar_generation_kw(state, new_solar_array):
 
 
 def power_usage_kw(outputs):
-    total_power_used_kw = 0.0
-    total_energy_used_kw = 0.0
-
     total_power_used_kw = outputs["co2_scrubber_power_used_kw"] + outputs["oga_power_used_kw"] + outputs["light_power_kw"]
     total_energy_used_kwh = outputs["co2_scrubber_energy_used_kwh"] + outputs["oga_energy_used_kwh"] + outputs["light_power_used_kwh"]
   
