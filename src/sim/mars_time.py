3ee57e01e21601fb59ddbc27a18f223332d1a81f
Adore2113 <adore.codes@gmail.com>
2026-04-05 19:39:38 -0400

    updated functions to work with Mars time better and added longitude and latitude.

--- src/sim/mars_time.py
+++ src/sim/mars_time.py
@@ -4,18 +4,35 @@ from .state import Habitat_State
 
 # file for handling all things time
 
+#    --------------------------------  
+
 total_sol_seconds = 88775     # one mars sol is 24h 39min 35sec
 sunrise_seconds = total_sol_seconds // 4   # ~ 22193
 afternoon_seconds = total_sol_seconds // 2   # ~ 44387
 sunset_seconds = (total_sol_seconds * 3) // 4    # ~ 66581
+
 max_daylight_m2_kw = 0.6    # placeholder
 
+# habitat location = 47° North, 184° East
+longitude_east_deg = 184
+longitude_north_deg = 47
 
-def sol_time(seconds):    # for a readable clock
-    sol_seconds = seconds % total_sol_seconds
+#    --------------------------------
+
+def time_in_sol_seconds(mission_time_s):
+    longitude_offset_s = (longitude_east_deg * total_sol_seconds) // 3 
+    return (mission_time_s + longitude_offset_s) % total_sol_seconds
+
+
+def sol_time(mission_time_s):    # for a readable clock
+    time_in_sol_s = time_in_sol_seconds(mission_time_s)
     
-    hour_24 = sol_seconds // 3600
-    minutes = (sol_seconds % 3600) // 60     # 1h = 60min, 1min = 60 sec, 60*60 = 3600
+    hour_24 = (time_in_sol_s * 24) // total_sol_seconds
+    hour_remainder = (time_in_sol_s * 24) % total_sol_seconds
+    
+    minutes = (hour_remainder * 60) // total_sol_seconds
+    minute_remainder = (hour_remainder * 60) % total_sol_seconds 
+    seconds = (minute_remainder * 60) // total_sol_seconds 
     
     meridiem = "AM"
     hour_12 = hour_24
@@ -27,17 +44,18 @@ def sol_time(seconds):    # for a readable clock
     if hour_24 == 0:
         hour_12 = 12
 
-    return hour_12, minutes, meridiem
+    return hour_12, minutes, seconds, meridiem
+
 
 
 def daylight_per_m2_kw(mission_time_s):
-    sol_seconds = mission_time_s % total_sol_seconds
-
-    if sol_seconds < sunrise_seconds or sol_seconds >= sunset_seconds:
+    time_in_sol_s = time_in_sol_seconds(mission_time_s)
+    
+    if time_in_sol_s < sunrise_seconds or time_in_sol_s >= sunset_seconds:
         return 0.0
     
-    daylight_length_seconds = sunset_seconds - sunrise_seconds
-    daylight_progress = (sol_seconds - sunrise_seconds) / daylight_length_seconds
+    daylight_length_s = sunset_seconds - sunrise_seconds
+    daylight_progress = (time_in_sol_s - sunrise_seconds) / daylight_length_s
 
     # using a sine wave, so using .sin from math import to get a radian and .pi 
     daylight_m2_kw = max_daylight_m2_kw * math.sin(math.pi * daylight_progress)
