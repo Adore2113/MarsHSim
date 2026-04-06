ae77e4a8d627b96bfbdd6582a6e83f5a51780361
Adore2113 <adore.codes@gmail.com>
2026-04-05 22:22:18 -0400

    organized notes.

--- src/sim/crew_metabolism.py
+++ src/sim/crew_metabolism.py
@@ -1,9 +1,7 @@
 from dataclasses import replace
 from .state import Habitat_State
 
-# file for handling crew metabolsim
-
-# ----crew activites and metabolism rates 
+# ----crew activites and metabolism rates-----------♡
 crew_activity_states = {
     "normal" : {"o2_drop_multiplier" : 1.0, "co2_rise_multiplier" : 1.0},
     "sleep" : {"o2_drop_multiplier" : 0.8, "co2_rise_multiplier" : 0.8},
@@ -11,12 +9,11 @@ crew_activity_states = {
     "intense" : {"o2_drop_multiplier" : 2.0, "co2_rise_multiplier" : 2.0},
 }
  
-
-# ----crew metabolism per default timestep----
+# ----crew metabolism per default timestep----------♡
 def crew_metabolism(state, dt_min):
     hours_per_step = dt_min / 60
     
-    # ----atmosphere gases----
+    # ----atmosphere gases----♡
     crew_activity = crew_activity_states[state.crew_activity]
     o2_drop_x = crew_activity["o2_drop_multiplier"]
     co2_rise_x = crew_activity["co2_rise_multiplier"]
@@ -24,7 +21,7 @@ def crew_metabolism(state, dt_min):
     o2_drop_kpa = 0.00011 * state.crew_count * o2_drop_x
     co2_rise_kpa = 0.0000967 * state.crew_count * co2_rise_x
 
-    # ----tempurature----
+    # ----tempurature rise----♡
     if state.crew_activity == "sleep":
         crew_temp_rise_kw = 0.083 * state.crew_count    # = 2.49
         crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
