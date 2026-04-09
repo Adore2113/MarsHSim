2fd3345448836073c346fe0a5420729b7f43345e
Adore2113 <adore.codes@gmail.com>
2026-04-09 13:09:42 -0400

    added constants from engine.py.

--- src/sim/temp_system.py
+++ src/sim/temp_system.py
@@ -3,3 +3,8 @@ from .state import Habitat_State
 
 # file for temperature and humidity
 
+#--------------------constants-----------------------♡
+target_temp_c = 23.0
+min_temp_c = 20.0
+max_temp_c = 25.0
+#----------------------------------------------------♡
