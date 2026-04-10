6e42927281218c87399de36f3d601bda6c8ee0be
Adore2113 <adore.codes@gmail.com>
2026-04-10 16:30:16 -0400

    renamed total  power usage function.

--- src/sim/power_system.py
+++ src/sim/power_system.py
@@ -111,7 +111,7 @@ def wellness_lights(state, dt_min):
     
 
 #-----how much power habitat systems are using-------♡
-def power_usage_kw(outputs):
+def total_power_usage(outputs):
     total_power_used_kw = outputs["co2_scrubber_power_used_kw"] + outputs["oga_power_used_kw"] + outputs["light_power_kw"] + outputs["w_light_power_used_kw"]
     total_energy_used_kwh = outputs["co2_scrubber_energy_used_kwh"] + outputs["oga_energy_used_kwh"] + outputs["light_power_used_kwh"] + outputs ["w_light_power_used_kwh"]
   
