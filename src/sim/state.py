eb88cd55b0a5c56080bd07d1701133514e847bfd
Adore2113 <adore.codes@gmail.com>
2026-04-15 17:16:05 -0400

    added radiators variable.

--- src/sim/state.py
+++ src/sim/state.py
@@ -19,6 +19,7 @@ class Habitat_State:
     hab_temp_c: float
     insulation_strength_kw_per_c: float
     thermal_mass_kwh_per_c: float
+    radiators: list
 
 # ------atmosphere targets and limits---------------♡ 
     target_pressure_kpa: float
