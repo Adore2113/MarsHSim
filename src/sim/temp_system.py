223e888db4de0d616f412530b4a6e3bfd832da60
Adore2113 <adore.codes@gmail.com>
2026-04-14 21:22:51 -0400

    added very basic heat loss from outside function.

--- src/sim/temp_system.py
+++ src/sim/temp_system.py
@@ -1,5 +1,6 @@
 from dataclasses import replace
 from .state import Habitat_State
+from .mars_time import current_mars_season, determine_sunlight_amount
 
 # file for temperature and humidity
 
@@ -21,20 +22,48 @@ def heat_from_outputs_kw(outputs):
     ) 
 
 #----------------get total humidity------------------♡
-
-
-#---------------internal temp system-----------------♡
-#----------------------radiators---------------------♡
-#------------------electric heaters------------------♡
+    # focusing on temp first
 
 
 #--------------external Mars environment-------------♡
+def determine_outside_temp_c(state):
+    season = current_mars_season(state)
+    sunlight = determine_sunlight_amount(state)
+
+    if season == "north_spring":
+        base_temp_c = -10.0
+        day_night_variation = 10.0
+
+    elif season == "north_summer":
+        base_temp_c = 0.0
+        day_night_variation = 12.0
+    
+    elif season == "north_autumn":
+        base_temp_c = -12.5
+        day_night_variation = 10.0
+    
+    else:
+        base_temp_c = -20.0
+        day_night_variation = 8.0
+
+    temp_offset = (sunlight - 0.5) * 2 * day_night_variation
+    outside_temp_c = base_temp_c + temp_offset
+    return outside_temp_c
 
 
-#--------------seasonal/weather changes--------------♡
+#------passive heat loss to outside environment------♡
+def heat_loss_from_outside_kw(state, outside_temp_c):
+    temp_difference_c = state.cabin_temp_c - outside_temp_c
+
+    if temp_difference_c <= 0:
+        return 0.0
+    
+    heat_loss_kw = temp_difference_c * state.insulation_loss
+
+    return heat_loss_kw
+
+#------------internal habitat temp changes-----------♡
+#def hab_heat_changes_kw(state, outputs):
+  #  internal_heat_kw = heat_from_outputs_kw(outputs)
 
 
-#-------calculate total temp changes generated-------♡
-
-
-#------------------run_temp_system-------------------♡
