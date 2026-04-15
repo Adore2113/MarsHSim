57cb169a1be95c847a5f1161fe7215b7e7dd08f6
Adore2113 <adore.codes@gmail.com>
2026-04-14 21:26:07 -0400

    added new variable for habitat insulation strength.

--- src/sim/quick_test.py
+++ src/sim/quick_test.py
@@ -18,6 +18,7 @@ s0 = Habitat_State(
 # ------habitat-------------------------------------♡
     hab_vol_m3 = 2000.0,
     hab_temp_c = 23.0,
+    insulation_strength = 20.0,    # subject to change
 
 # ------atmosphere targets and limits---------------♡ 
     target_pressure_kpa = 60.0,
