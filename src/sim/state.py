c50f439e3d7bc44290cf368101c23938882c9eb8
Adore2113 <adore.codes@gmail.com>
2026-04-20 16:02:17 -0400

    added target_humidity_pct varialbe.

--- src/sim/state.py
+++ src/sim/state.py
@@ -17,6 +17,7 @@ class Habitat_State:
 
 # ------thermal control-----------------------------♡
     hab_temp_c: float
+    target_humidity_pct: float
     insulation_strength_kw_per_c: float
     thermal_mass_kwh_per_c: float
     radiators: list
