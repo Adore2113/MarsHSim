d866fbfcec049b5a96871865b48b0721fcb5f85a
Adore2113 <adore.codes@gmail.com>
2026-04-20 15:21:04 -0400

    updated file and added breath and skin vapor variables.

--- src/sim/crew_metabolism.py
+++ src/sim/crew_metabolism.py
@@ -3,48 +3,35 @@ from .state import Habitat_State
 
 #--------crew activites and metabolism rates---------♡
 crew_activity_states = {
-    "normal" : {"o2_drop_multiplier" : 1.0, "co2_rise_multiplier" : 1.0},
-    "sleep" : {"o2_drop_multiplier" : 0.8, "co2_rise_multiplier" : 0.8},
-    "exercise" : {"o2_drop_multiplier" : 1.5, "co2_rise_multiplier" : 1.5},  
-    "intense" : {"o2_drop_multiplier" : 2.0, "co2_rise_multiplier" : 2.0},
+    "normal" : {"o2_drop_multiplier" : 1.0, "co2_rise_multiplier" : 1.0, "breath_vapor_multiplier" : 1.0, "skin_vapor_multiplier" : 1.0, "heat_per_person_w" : 120},
+    "sleep" : {"o2_drop_multiplier" : 0.8, "co2_rise_multiplier" : 0.8, "breath_vapor_multiplier" : 0.8, "skin_vapor_multiplier" : 0.4, "heat_per_person_w" : 83},
+    "exercise" : {"o2_drop_multiplier" : 1.5, "co2_rise_multiplier" : 1.5, "breath_vapor_multiplier" : 1.4, "skin_vapor_multiplier" : 2.0, "heat_per_person_w" : 280},  
+    "intense" : {"o2_drop_multiplier" : 2.0, "co2_rise_multiplier" : 2.0, "breath_vapor_multiplier" : 1.8, "skin_vapor_multiplier" : 3.0, "heat_per_person_w" : 350},
     }
-#----------------------------------------------------♡
 
 
 # -------crew metabolism per default timestep--------♡
 def crew_metabolism(state, dt_min):
     hours_per_step = dt_min / 60
-    
-    
-    #-------------atmosphere gas changes-------------♡
     crew_activity = crew_activity_states[state.crew_activity]
-    o2_drop_x = crew_activity["o2_drop_multiplier"]
-    co2_rise_x = crew_activity["co2_rise_multiplier"]
-    
-    o2_drop_kpa = 0.00011 * state.crew_count * o2_drop_x
-    co2_rise_kpa = 0.0000967 * state.crew_count * co2_rise_x
 
+    #-------------atmosphere gas changes-------------♡
+    o2_drop_kpa = 0.00011 * state.crew_count * crew_activity["o2_drop_multiplier"]
+    co2_rise_kpa = 0.0000967 * state.crew_count * crew_activity["co2_rise_multiplier"]
+
+    #----------------humidity changes----------------♡
+    breath_vapor_added_kg = (1.0 * state.crew_count * crew_activity["breath_vapor_multiplier"] * hours_per_step) / 24
+    skin_vapor_added_kg = (0.8 * state.crew_count * crew_activity["skin_vapor_multiplier"] * hours_per_step) / 24
 
     #--------------temperature changes---------------♡
-    if state.crew_activity == "sleep":
-        crew_temp_rise_kw = 0.083 * state.crew_count    # = 2.49
-        crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
-
-    elif state.crew_activity == "exercise":   
-        crew_temp_rise_kw = 0.280 * state.crew_count    # = 9.0
-        crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
-    
-    elif state.crew_activity == "intense":
-        crew_temp_rise_kw = 0.350 * state.crew_count    # = 10.5
-        crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
-    
-    else: 
-        crew_temp_rise_kw = 0.120 * state.crew_count    # = 3.78
-        crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
+    crew_temp_rise_kw = (crew_activity["heat_per_person_w"] * state.crew_count) / 1000.0
+    crew_temp_rise_kwh = crew_temp_rise_kw * hours_per_step
 
     return {
         "o2_drop_kpa" : o2_drop_kpa,
         "co2_rise_kpa" : co2_rise_kpa,
         "crew_temp_rise_kw" : crew_temp_rise_kw,
-        "crew_temp_rise_kwh" : crew_temp_rise_kwh
+        "crew_temp_rise_kwh" : crew_temp_rise_kwh,
+        "breath_vapor_added_kg" : breath_vapor_added_kg,
+        "skin_vapor_added_kg" : skin_vapor_added_kg
         }
