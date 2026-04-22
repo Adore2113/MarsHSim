1e0bf9ef0fb12070a328d6674839048ab3e0ce52
Adore2113 <adore.codes@gmail.com>
2026-04-22 17:35:19 -0400

    added global constants for tank capacities with placeholder values and started to calculate crew water changes.

--- src/sim/water_system.py
+++ src/sim/water_system.py
@@ -15,42 +15,51 @@ base_bpa_power_kw = 0.75
 upa_handling_capacity_per_hour_kg = 6.0
 wpa_handling_capacity_per_hour_kg = 10.0
 bpa_handling_capacity_per_hour_kg = 0.15
+
+potable_water_tank_capacity_kg = 5000.0    #placeholder
+gray_water_tank_capacity_kg = 500.0    # placeholder!
+black_water_tank_capacity_kg = 300.0    # placeholder
 #----------------------------------------------------♡
 
 
 
-#-------calculate how much water crew has used-------♡
-def crew_water_usage_kg(state):
-    total_crew_water_usage_kg = crew_gray_water_kg + laundry_wastewater_kg + hygiene_wastewater_kg + crew_Water_consumed_kg
+#------------calculate crew water changes------------♡
+def get_crew_water_usage(state, crew_results, dt_min):
+    hours_per_step = dt_min / 60
 
-    return total_crew_water_usage_kg
+    potable_water_used_kg = crew_results.get("potable_water_used_kg", 0.0)
+    gray_water_added_kg = crew_results.get("gray_water_added_kg", 0.0)
+    black_water_added_kg = crew_results.get("black_water_added_kg", 0.0)
+
+    new_potable_water_used_kg = max(0.0, state.potable_water_tank_kg - potable_water_used_kg)
+    new_gray_water_added_kg = min(state.gray_water_tank_kg * gray_water_added_kg, gray_water_tank_capacity_kg)    # placeholder!
+    new_black_water_added_kg = min(state.black_water_tank_kg * black_water_added_kg, black_water_tank_capacity_kg)
+
+
+
+
+
+
+
 
 #------------run urine processor assembly------------♡
 def run_upa(state, dt_min):
     hours_per_step = dt_min / 60
-    upa_base_power_kw = ...
-    total_wastewater_produced_kg = crew.wastewater_added_kg * hours_per_step
     ...
 
 
 #------------run brine processor assembly------------♡
 def run_bpa(state, dt_min):
     hours_per_step = dt_min / 60
-    bpa_base_power_kw = ...
     ...
 
 
 #------------run water processor assembly------------♡
 def run_wpa(state, dt_min):
     hours_per_step = dt_min / 60
-    wpa_base_power_kw = ...
     ...
 
 
 #---------------update water storage-----------------♡
 def update_water_tanks_kg(state, total_crew_water_usage_kg):
-    total_water_usage_kg = oga_water_consumed + 
-
-    new_water_stored = state.water_tank_kg - total_water_usage_kg
-
-    return new_water_stored
+    ...
