864771d1243eb8ccb191daa1e5c42fda59922324
Adore2113 <adore.codes@gmail.com>
2026-04-22 23:02:08 -0400

    added gray_water_removed_kg from run_wpa dictionary, but paired it with water_processed_kg for now until I get condensate_kg figured out.

--- src/sim/water_system.py
+++ src/sim/water_system.py
@@ -102,12 +102,12 @@ def run_bpa(state, dt_min):
 
 
 #------------run water processor assembly------------♡
-def run_wpa(state, condensate_kg, dt_min):
+def run_wpa(state, dt_min, condensate_kg):
     hours_per_step = dt_min / 60
-    total_water_input_kg = state.gray_water_storage_kg #+ condensate_kg
+    total_water_input_kg = state.gray_water_storage_kg + condensate_kg
     
     if total_water_input_kg <= 0.1:
-        return {"recovered_water_kg" : 0.0, "water_processed_kg": 0.0, "wpa_power_used_kw" : 0.0, "wpa_energy_used_kwh" : 0.0}
+        return {"recovered_water_kg" : 0.0, "water_processed_kg": 0.0, "gray_water_removed_kg": 0.0, "wpa_power_used_kw" : 0.0, "wpa_energy_used_kwh" : 0.0}
 
     water_processed_kg = min(total_water_input_kg, wpa_handling_capacity_per_hour_kg * hours_per_step)
     recovered_water_kg = water_processed_kg * wpa_recovery_rate
@@ -123,6 +123,8 @@ def run_wpa(state, condensate_kg, dt_min):
     return {
         "recovered_water_kg": recovered_water_kg,
         "water_processed_kg": water_processed_kg,
+        "gray_water_removed_kg": water_processed_kg,    # change after figuring out condensate_kg
+
         "wpa_power_used_kw": wpa_power_used_kw,
         "wpa_energy_used_kwh": wpa_energy_used_kwh,
     }
