bdc26b3821b570502e353f6575092798147e7e2f
Adore2113 <adore.codes@gmail.com>
2026-04-24 16:29:41 -0400

    added global varialbes and imported seconds_per_sol from mars_time.py.

--- src/sim/dust_system.py
+++ src/sim/dust_system.py
@@ -1,13 +1,18 @@
 from dataclasses import replace
 from .state import Habitat_State
+from .mars_time import seconds_per_sol
 
 # file for all things Mars dust
 
 #--------------------constants-----------------------♡
 dust_rate_per_sol = 0.007
 
-radiator_dust_mulitplier = 0.035   # placeholder
+primary_radiator_dust_mulitplier = 1.1   # placeholder
+backup_radiator_dust_multiplier = 0.9   # placeholder
 solar_dust_miltiplier = 0.40   # placeholder
+
+min_raditor_efficiency = 0.35   # placeholder
+min_solar_effiency = 0.40   # placeholder
 #----------------------------------------------------♡
 
 
