65c71d97d60fc4951fb9ab6a9b45e2c9a8c08cd3
Adore2113 <adore.codes@gmail.com>
2026-04-24 16:24:23 -0400

    created file for handling dust.

---
+++ src/sim/dust_system.py
@@ -0,0 +1,16 @@
+from dataclasses import replace
+from .state import Habitat_State
+
+# file for all things Mars dust
+
+#--------------------constants-----------------------♡
+dust_rate_per_sol = 0.007
+
+radiator_dust_mulitplier = 0.035   # placeholder
+solar_dust_miltiplier = 0.40   # placeholder
+#----------------------------------------------------♡
+
+
+#-----------------dust accumulation------------------♡
+def get_total_dust_accumulation(state, ... ):
+    ...
