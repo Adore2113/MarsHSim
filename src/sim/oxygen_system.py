03716fd9aa47f1eb251f3c9c6e25f60ccb72877a
Adore2113 <adore.codes@gmail.com>
2026-04-24 21:55:45 -0400

    organized file.

--- src/sim/oxygen_system.py
+++ src/sim/oxygen_system.py
@@ -1,6 +1,3 @@
-from dataclasses import replace
-from .state import Habitat_State
-
 # file for Oxygen Generation Assembly (OGA) & Water Electrolysis
 
 #--------------------constants-----------------------♡
