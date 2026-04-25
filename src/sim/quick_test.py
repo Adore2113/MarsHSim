ab0dd2c1ab5b3b9e111264757a8da436845bab3d
Adore2113 <adore.codes@gmail.com>
2026-04-24 20:44:55 -0400

    organized file, removed some placeholders and updated amine beds to match radiators/heaters.

--- src/sim/quick_test.py
+++ src/sim/quick_test.py
@@ -1,25 +1,32 @@
+#--------------------imports-------------------------♡
 from src.sim.state import Habitat_State
 from src.sim.engine import step
 from src.sim.mars_time import get_sol_time
 from .buffer_gas_system import mca
 from .alerts import get_alerts, get_status
 from .temp_system import get_thermal_alerts
+#----------------------------------------------------♡
+
 
 s0 = Habitat_State(
-# ------time----------------------------------------♡
+    #---------------time and daylight----------------♡
     mission_time_s = 0,
+
+    daylight_m2_kw = 0.0,
+    peak_sunlight_today = 0.0,
+    low_sunlight_streak_sols = 0,
     
-# ------lights--------------------------------------♡
+    #--------------------lights----------------------♡
     light_level = 0.0,
 
-# ------crew----------------------------------------♡
+    #---------------------crew-----------------------♡
     crew_count = 30,
     crew_activity = "normal",
   
-# ------habitat-------------------------------------♡
+    #--------------------habitat---------------------♡
     hab_vol_m3 = 2000.0,
 
-# ------thermal control-----------------------------♡
+    #--------------------thermal---------------------♡
     hab_temp_c = 23.0,
     target_humidity_pct = 48.0,
     current_humidity_pct = 48.0,
@@ -48,7 +55,7 @@ s0 = Habitat_State(
         {"id" : 6, "status" : "standby", "power_kw" : 8.0, "efficiency" : 0.98, "type" : "backup"},
     ],
     
-# ------atmosphere targets and limits---------------♡ 
+    #---------atmosphere targets and limits----------♡
     target_pressure_kpa = 60.0,
     min_safe_pressure_kpa = 55.0,
     max_safe_pressure_kpa = 70.0,
@@ -58,31 +65,31 @@ s0 = Habitat_State(
     target_n2_kpa = 17.0,
     target_ar_kpa = 22.6,
 
-# ------current atmosphere--------------------------♡
+    #---------------current atmosphere---------------♡
     o2_kpa = 20.0,
     co2_kpa = 0.4,
     n2_kpa = 18.0,
     ar_kpa = 21.6, 
 
-# ------gas storage---------------------------------♡
+    #------------------gas storage-------------------♡
     n2_stored_kpa = 60.0,   # ~1365 kg
     ar_stored_kpa = 30.0,   # ~973.5 kg
     co2_stored_kpa = 0.0,   # temporarily putting the co2 that the scrubber removes to here
     h2_stored_kg = 0.0,  
 
-# ------amine beds----------------------------------♡
+    #------------------amine_beds--------------------♡
     amine_beds = [
-        {"id" : 1, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0},
-        {"id" : 2, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0},
-        {"id" : 3, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0},
-        {"id" : 4, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0}
-    #    {"id" : 5, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0},
-    #    {"id" : 6, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0}
-  
+        {"id" : 1, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0, "type": "primary"},
+        {"id" : 2, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0, "type": "primary"},
+        {"id" : 3, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0, "type": "primary"},
+        {"id" : 4, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0, "type": "primary"},
+        
+        {"id" : 5, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0, "type": "backup"},
+        {"id" : 6, "status" : "standby", "capacity" : 3.0, "co2_load" : 0.0, "type": "backup"},
     ],
     scrub_per_bed_kpa = 0.0035,
 
-# ------power---------------------------------------♡
+    #-----------------power / solar------------------♡
     battery_max_capacity_kwh = 4000.0,
     battery_stored_kwh = 3800.0,    # starting with max capacity, for now
     
@@ -99,36 +106,30 @@ s0 = Habitat_State(
         {"id" : 10,"status" : "standby", "area_m2" : 50, "efficiency" : 0.28, "dust_factor" : 1.0}
     ], 
     
-    daylight_m2_kw = 0.0,
-    peak_sunlight_today = 0.0,
-    low_sunlight_streak_sols = 0,
     solar_absorptivity = 0.68,
     
-# ------water---------------------------------------♡
-
+    #---------------------water----------------------♡
     potable_water_storage_kg = 5000.0,
     gray_water_storage_kg = 0.0,
     black_water_storage_kg = 0.0,
     condensate_storage_kg = 0.0,
     brine_storage_kg = 0.0,
 
-    potable_water_storage_capacity_kg = 5000.0,    # placeholder
-    gray_water_storage_capacity_kg = 500.0,    # placeholder
-    black_water_storage_capacity_kg = 300.0,    # placeholder
-    condensate_storage_capacity_kg = 80.0,    # placeholder
-    brine_storage_capacity_kg = 150.0,    # placeholder
+    potable_water_storage_capacity_kg = 6500.0,
+    gray_water_storage_capacity_kg = 1200.0,
+    black_water_storage_capacity_kg = 800.0,
+    condensate_storage_capacity_kg = 250.0,
+    brine_storage_capacity_kg = 400.0,
 
-# ------placeholders for future plans---------------♡
-
-    # ♡ Integrity / safety ♡
+    #------------------placeholders------------------♡
     leak_rate_kpa_per_hr = 0.0,
     smoke_ppm = 0.0,
     radiation_msv_per_day = 0.7,
 
-# --------wellness lights on or off-----------------♡
+    #----------------wellness lights-----------------♡
     wellness_lights_on = False,
     )
-#----------------------------------------------------♡
+    #------------------------------------------------♡
 
 
 def print_state(state, outputs, alerts):
