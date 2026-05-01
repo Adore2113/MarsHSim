59daeded44c22be3cf4f4f5a2bbbb3122e210855
Adore2113 <adore.codes@gmail.com>
2026-04-30 22:09:44 -0400

    added modes, constants and conversaion variables to sabatier_system.py.

--- src/sim/sabatier_system.py
+++ src/sim/sabatier_system.py
@@ -2,17 +2,56 @@
 
 
 #--------------------constants-----------------------♡
-base_sab    # sabatier
-wpa_recovery_rate = 0.95    # water processor assembly
-bpa_recovery_rate = 0.60    # brine processor assembly
+kelvin_offset = 273.15   # add to celsius to convert to kelvin
+r_kpa = 0.008314   # the universal gas constant, 8.314 / 1000 
+pa_per_kpa = 1000.0   # 1kpa = 1000pa
 
-base_upa_power_kw = 0.45
-base_wpa_power_kw = 0.35
-base_bpa_power_kw = 0.75
-sab_base_power_kw = 0.8
+h2_molar_mass = 2.016   # 1 mole h2 = 2.016g b/c h2 = 2 hydrogen atoms (1.008 g/mol each)
+o2_molar_mass = 32.0   # grams per mole
+co2_molar_mass = 44.01
+ch4_molar_mass = 16.043
+h2o_molar_mass = 18.015
 
-upa_handling_capacity_per_hour_kg = 6.0
-wpa_handling_capacity_per_hour_kg = 10.0
-bpa_handling_capacity_per_hour_kg = 0.15
-sab_handling_capacity_per_hour_kg = 2.0
+h2_to_water_ratio = 0.45
+
+min_h2_for_reaction_kg = 0.05
+min_co2_for_reaction_kpa = 0.05
+
+base_sabatier_power_kw = 0.85
+base_sabatier_temp_c = 300.0
+base_sabatier_efficiency = 0.75
+
+hysteresis_kpa = 0.05
 #----------------------------------------------------♡
+
+
+#-------------convert co2 from kpa to kg-------------♡
+def run_conversions(state):
+    temp_k = state.hab_temp_c + kelvin_offset
+    co2_kg = (state.co2_kpa * state.hab_vol_m3 * co2_molar_mass) / (r_kpa * temp_k * 1000)
+
+    return temp_k, co2_kg
+#----------------co2 + h2 = ch4 + H2o----------------♡
+def run_sabatier(state, dt_min, co2_kg):
+    hours_per_step = dt_min / 60
+    current_h2_kg = state.h2_kg
+    current_co2_kg = co2_kg
+
+    #----------------sabatier modes-----------------♡  
+    if not state.sabatier_on:
+        sabatier_mode = "offline"
+
+    elif current_co2_kg <= min_co2_for_reaction_kpa and current_h2_kg <= min_h2_for_reaction_kg:
+        sabatier_mode = "idle"
+    
+    elif current_co2_kg <= min_co2_for_reaction_kpa:
+        sabatier_mode = "idle, limited co2"
+    
+    elif current_h2_kg <= min_h2_for_reaction_kg:
+        sabatier_mode = "idle, limited h2"
+    
+    else:
+        sabatier_mode = "running"
+
+    if sabatier_mode =  "running":
+        ...
