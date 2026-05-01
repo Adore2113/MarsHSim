fe4059cccdedd28c012f7354ab93b2bae0820b41
Adore2113 <adore.codes@gmail.com>
2026-04-30 22:48:11 -0400

    added new constants for conversions in sabatier_system.py and started the chemistry.

--- src/sim/sabatier_system.py
+++ src/sim/sabatier_system.py
@@ -4,7 +4,9 @@
 #--------------------constants-----------------------♡
 kelvin_offset = 273.15   # add to celsius to convert to kelvin
 r_kpa = 0.008314   # the universal gas constant, 8.314 / 1000 
-pa_per_kpa = 1000.0   # 1kpa = 1000pa
+pa_per_kpa = 1000.0
+kg_per_g = 0.001
+exothermic_reaction = 0.65
 
 h2_molar_mass = 2.016   # 1 mole h2 = 2.016g b/c h2 = 2 hydrogen atoms (1.008 g/mol each)
 o2_molar_mass = 32.0   # grams per mole
@@ -21,7 +23,7 @@ base_sabatier_power_kw = 0.85
 base_sabatier_temp_c = 300.0
 base_sabatier_efficiency = 0.75
 
-hysteresis_kpa = 0.05
+hysteresis = 1.5
 #----------------------------------------------------♡
 
 
@@ -34,24 +36,36 @@ def run_conversions(state):
 #----------------co2 + h2 = ch4 + H2o----------------♡
 def run_sabatier(state, dt_min, co2_kg):
     hours_per_step = dt_min / 60
-    current_h2_kg = state.h2_kg
-    current_co2_kg = co2_kg
+    h2_kg = state.h2_kg
+    _, co2_kg = run_conversions(state)
 
     #----------------sabatier modes-----------------♡  
     if not state.sabatier_on:
         sabatier_mode = "offline"
 
-    elif current_co2_kg <= min_co2_for_reaction_kpa and current_h2_kg <= min_h2_for_reaction_kg:
+    elif co2_kg <= min_co2_for_reaction_kpa and h2_kg <= min_h2_for_reaction_kg:
         sabatier_mode = "idle"
     
-    elif current_co2_kg <= min_co2_for_reaction_kpa:
+    elif co2_kg <= min_co2_for_reaction_kpa * hysteresis:
         sabatier_mode = "idle, limited co2"
     
-    elif current_h2_kg <= min_h2_for_reaction_kg:
+    elif h2_kg <= min_h2_for_reaction_kg * hysteresis:
         sabatier_mode = "idle, limited h2"
     
     else:
         sabatier_mode = "running"
 
-    if sabatier_mode =  "running":
-        ...
+    #---------------running sabatier----------------♡  
+    if sabatier_mode == "running":
+        h2_moles = h2_kg / h2_molar_mass
+        co2_moles = co2_kg / co2_molar_mass
+
+        reactions_avaliable = min(h2_moles / 4, co2_moles) * base_sabatier_efficiency     # 1 co2 : 4 h2
+
+        water_produced_kg = reactions_avaliable * 2 * h2o_molar_mass * kg_per_g
+        ch4_produced_kg = reactions_avaliable * 1 * ch4_molar_mass * kg_per_g
+        h2_consumed_kg = reactions_avaliable * 4  * h2_molar_mass * kg_per_g
+        co2_consumed_kg = reactions_avaliable * 1 * co2_molar_mass * kg_per_g
+
+        sabatier_power_used_kw = base_sabatier_power_kw
+        sabatier_heat_added_kw = base_sabatier_power_kw * exothermic_reaction
