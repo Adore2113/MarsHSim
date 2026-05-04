8e7a9a896a474e9429a145ef89e36d442da3f783
Adore2113 <adore.codes@gmail.com>
2026-05-04 00:15:17 -0400

    adding a slight gas leak when the buffer gas system is running, in buffer_gas_system.py.

--- src/sim/buffer_gas_system.py
+++ src/sim/buffer_gas_system.py
@@ -19,7 +19,9 @@ def mca(state):
 
 
 #--------------stabilizing gas levels---------------♡
-def buffer_gas_control_kpa(state):
+def buffer_gas_control_kpa(state, dt_min):
+    hours_per_step = dt_min / 60
+
     new_n2_kpa = state.n2_kpa
     new_ar_kpa = state.ar_kpa
     new_n2_stored_kg = state.n2_stored_kg
@@ -27,7 +29,8 @@ def buffer_gas_control_kpa(state):
 
     total_pressure_kpa = mca(state)
     pressure_gap_kpa = state.target_pressure_kpa - total_pressure_kpa
-    
+
+    #----------------default values-----------------♡  
     total_buffer_gas_added_kpa = 0.0
     total_buffer_gas_vented_kpa = 0.0
 
@@ -46,11 +49,12 @@ def buffer_gas_control_kpa(state):
 
     else:
         buffer_gas_mode = "stable"
-        
+
+    #------------------adding gas-------------------♡  
     if buffer_gas_mode in ("emergency_add", "add"):
         pressure_to_add_kpa = pressure_needed_kpa
-
-    #------------handling Nitrogen first------------♡  
+        
+        #--------------Nitrogen first---------------♡  
         if new_n2_kpa < state.target_n2_kpa:
             n2_room_left_kpa = state.target_n2_kpa - new_n2_kpa
             n2_to_add_kpa = min(pressure_to_add_kpa, n2_room_left_kpa, new_n2_stored_kg)
@@ -61,7 +65,7 @@ def buffer_gas_control_kpa(state):
             total_buffer_gas_added_kpa += n2_to_add_kpa
             pressure_to_add_kpa -= n2_to_add_kpa
 
-    #----------------handling Argon-----------------♡  
+        #----------------Argon second---------------♡
         if pressure_to_add_kpa > 0 and new_ar_kpa < state.target_ar_kpa:
             ar_room_left_kpa = state.target_ar_kpa - new_ar_kpa
             ar_to_add_kpa = min(pressure_to_add_kpa, ar_room_left_kpa, new_ar_stored_kg)
@@ -74,12 +78,13 @@ def buffer_gas_control_kpa(state):
     #---------------venting extra gas---------------♡  
     elif buffer_gas_mode == "vent":
         pressure_to_vent_kpa = -pressure_gap_kpa
+        total_buffer_gas_vented_kpa = 0.0
         
         #-------------vent Argon first--------------♡  
         if new_ar_kpa > state.target_ar_kpa:
             ar_to_vent = min(pressure_to_vent_kpa, new_ar_kpa - state.target_ar_kpa)
             new_ar_kpa -= ar_to_vent
-            pressure_to_vent_kpa -= ar_to_vent
+            pressure_to_vent_kpa -= (ar_to_vent * 0.8)    # 8% leaks back
             
             total_buffer_gas_vented_kpa += ar_to_vent 
   
@@ -87,6 +92,7 @@ def buffer_gas_control_kpa(state):
         if pressure_to_vent_kpa > 0 and new_n2_kpa > state.target_n2_kpa:
             n2_to_vent = min(pressure_to_vent_kpa, new_n2_kpa - state.target_n2_kpa)
             new_n2_kpa -= n2_to_vent
+            new_n2_kpa += (n2_to_vent * 0.02)
             
             total_buffer_gas_vented_kpa += n2_to_vent
             
@@ -122,7 +128,7 @@ def buffer_gas_power_and_heat(total_buffer_gas_added_kpa, total_buffer_gas_vente
 
 #-------buffer gas control info per timestep--------♡
 def run_buffer_gas_control(state, dt_min):
-    n2_kpa, ar_kpa, n2_stored_kg, ar_stored_kg, total_buffer_gas_added_kpa, total_buffer_gas_vented_kpa, buffer_gas_mode, pressure_gap_kpa = buffer_gas_control_kpa(state)
+    n2_kpa, ar_kpa, n2_stored_kg, ar_stored_kg, total_buffer_gas_added_kpa, total_buffer_gas_vented_kpa, buffer_gas_mode, pressure_gap_kpa = buffer_gas_control_kpa(state, dt_min)
     
     buffer_gas_heat_added_kw, buffer_gas_heat_added_kwh, buffer_gas_power_used_kw, buffer_gas_energy_used_kwh = buffer_gas_power_and_heat(total_buffer_gas_added_kpa, total_buffer_gas_vented_kpa, dt_min)
     
