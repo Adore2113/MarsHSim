9c1a1570d4dd920aead4ab2adc8eaa111ebece73
Adore2113 <adore.codes@gmail.com>
2026-05-06 15:34:54 -0400

    cleaned up file, updated logic in run_system_power function, and put wellness lights and light system together in one function in  power_system.py.

--- src/sim/power_system.py
+++ src/sim/power_system.py
@@ -6,7 +6,7 @@ from .mars_time import get_sol_time, determine_sunlight_amount
 #--------------------constants-----------------------♡
 min_arrays_online = 6
 max_arrays_online = 10
-target_power_usage_ratio = 0.85
+min_light_level = 0.2
 
 base_light_power_kw = 2.0
 base_light_heat_kw = 0.5
@@ -101,11 +101,14 @@ def solar_battery_charge(state, total_solar_generated_kwh):
 
 
 #-----------habitat main light power info------------♡
-def lights(state, dt_min):
+def light_system(state, dt_min):
     hours_per_step = dt_min / 60
     _, sol_hour, minutes = get_sol_time(state)
-    sunlight_amount = determine_sunlight_amount(state)
     
+    sunlight_amount = determine_sunlight_amount(state)
+    low_sunlight_streak = state.low_sunlight_streak_sols
+
+    #------------------main lights------------------♡ 
     crew_awake_hours = 6 <= sol_hour < 21 or (sol_hour == 21 and minutes < 30)
 
     if crew_awake_hours:
@@ -116,30 +119,12 @@ def lights(state, dt_min):
 
     sunlight_dimming = sunlight_amount * 0.6    # sunlight level changes light level need for power saving
     light_level_dimmed = base_light_level - sunlight_dimming
-    
     final_light_level = max(min_light_level, light_level_dimmed)
 
     light_power_used_kw = base_light_power_kw * final_light_level
-    light_energy_used_kwh = light_power_used_kw * hours_per_step
-
     light_heat_kw =  base_light_heat_kw* final_light_level
-    light_heat_kwh = light_heat_kw * hours_per_step
 
-    return {
-        "final_light_level": final_light_level,
-        "light_heat_kw": light_heat_kw,
-        "light_heat_kwh": light_heat_kwh,
-        "light_power_used_kw": light_power_used_kw,
-        "light_energy_used_kwh": light_energy_used_kwh
-        }
-
-
-#--------wellness lights from lack of sunlight-------♡
-def wellness_lights(state, dt_min):
-    hours_per_step = dt_min / 60
-    low_sunlight_streak = state.low_sunlight_streak_sols
-
-    #----------------hysteresis----------------------♡
+    #----------------wellness lights----------------♡ 
     if low_sunlight_streak >= 3:
         wellness_lights_on = True
         wellness_light_level = 1.0
@@ -150,65 +135,48 @@ def wellness_lights(state, dt_min):
     
     else:
         wellness_lights_on = state.wellness_lights_on
-    
-    w_light_power_used_kw = base_w_light_power_kw * wellness_light_level
-    w_light_energy_used_kwh = w_light_power_used_kw * hours_per_step
+        
+        if wellness_lights_on:
+            wellness_light_level = 1.0
 
+        else:
+            wellness_light_level = 0.0
+
+    w_light_power_used_kw = base_w_light_power_kw * wellness_light_level
     w_light_heat_kw =  base_w_light_heat_kw * wellness_light_level
-    w_light_heat_kwh = w_light_heat_kw * hours_per_step
+
+    #------------total light heat added-------------♡ 
+    total_light_heat_kw = light_heat_kw + w_light_heat_kw
 
     return {
-            "wellness_lights_on": wellness_lights_on,
-            "wellness_light_level": wellness_light_level,
-            "w_light_power_used_kw": w_light_power_used_kw,
-            "w_light_energy_used_kwh": w_light_energy_used_kwh,
-            "w_light_heat_kw": w_light_heat_kw,
-            "w_light_heat_kwh": w_light_heat_kwh
-        }    
+        "final_light_level": final_light_level,
+        "light_heat_kw": light_heat_kw,
+        "light_heat_kwh": light_heat_kw * hours_per_step,
+        "light_power_used_kw": light_power_used_kw,
+        "light_energy_used_kwh": light_power_used_kw * hours_per_step,
+
+        "wellness_lights_on": wellness_lights_on,
+        "wellness_light_level": wellness_light_level,
+        "w_light_power_used_kw": w_light_power_used_kw,
+        "w_light_energy_used_kwh":  w_light_power_used_kw * hours_per_step,
+        "w_light_heat_kw": w_light_heat_kw,
+        "w_light_heat_kwh": w_light_heat_kw * hours_per_step,
+
+        "total_light_heat_kw": total_light_heat_kw,
+        "total_light_heat_kwh": total_light_heat_kw * hours_per_step,
+        }
 
 
 #------------------total power usage-----------------♡
-def get_total_power_usage(
-        co2_scrubber_power_used_kw, co2_scrubber_energy_used_kwh,
-        oga_power_used_kw, oga_energy_used_kwh,
-        light_power_used_kw, light_energy_used_kwh,
-        w_light_power_used_kw, w_light_energy_used_kwh,
-        radiator_power_kw, radiator_energy_kwh,
-        heater_power_kw, heater_energy_kwh,
-        chx_power_used_kw, chx_energy_used_kwh
-    ):
-    
-    total_power_used_kw = (
-        + co2_scrubber_power_used_kw
-        + oga_power_used_kw
-        + light_power_used_kw
-        + w_light_power_used_kw
-        + radiator_power_kw
-        + heater_power_kw
-        + chx_power_used_kw
-        )
-    
-    total_energy_used_kwh = (
-        + co2_scrubber_energy_used_kwh
-        + oga_energy_used_kwh
-        + light_energy_used_kwh
-        + w_light_energy_used_kwh
-        + radiator_energy_kwh
-        + heater_energy_kwh
-        + chx_energy_used_kwh
-        )  
+def get_total_power_usage(co2_scrubber_power_used_kw, oga_power_used_kw, light_power_used_kw, w_light_power_used_kw, radiator_power_kw, heater_power_kw, chx_power_used_kw):
+    total_power_used_kw = (co2_scrubber_power_used_kw + oga_power_used_kw + light_power_used_kw + w_light_power_used_kw + radiator_power_kw + heater_power_kw + chx_power_used_kw )
 
-    return total_power_used_kw, total_energy_used_kwh
+    return total_power_used_kw
 
+def get_total_energy_usage(co2_scrubber_energy_used_kwh, oga_energy_used_kwh, light_energy_used_kwh, w_light_energy_used_kwh, radiator_energy_kwh, heater_energy_kwh, chx_energy_used_kwh):
+    total_energy_used_kwh = (co2_scrubber_energy_used_kwh + oga_energy_used_kwh + light_energy_used_kwh + w_light_energy_used_kwh + radiator_energy_kwh + heater_energy_kwh + chx_energy_used_kwh )  
 
-#-----------------total heat generated---------------♡
-def total_heat_generated(light_heat_kw, light_heat_kwh ,w_light_heat_kw, w_light_heat_kwh):
-    total_heat_added_kw = light_heat_kw + w_light_heat_kw
-
-    total_heat_added_kwh =  light_heat_kwh + w_light_heat_kwh
-
-    return total_heat_added_kw, total_heat_added_kwh
-
+    return total_energy_used_kwh
 
 #------------------full power system-----------------♡
 def run_system_power(
@@ -216,7 +184,6 @@ def run_system_power(
     co2_results,
     oga_results,
     light_results,
-    wellness_results,
     thermal_outputs,
     humidity_results,
     dt_min):
@@ -227,24 +194,29 @@ def run_system_power(
     
     battery_after_charge = solar_battery_charge(state, total_solar_generated_kwh)
     
-    total_power_used_kw, total_energy_used_kwh = get_total_power_usage(
-        co2_results["co2_scrubber_power_used_kw"], co2_results["co2_scrubber_energy_used_kwh"],
-        oga_results["oga_power_used_kw"], oga_results["oga_energy_used_kwh"],
-        light_results["light_power_used_kw"], light_results["light_energy_used_kwh"],
-        wellness_results["w_light_power_used_kw"], wellness_results["w_light_energy_used_kwh"],
-        thermal_outputs.get("radiator_power_kw", 0.0), thermal_outputs.get("radiator_energy_kwh", 0.0),
-        thermal_outputs.get("heater_power_kw", 0.0), thermal_outputs.get("heater_energy_kwh", 0.0),
-        humidity_results.get("chx_power_used_kw", 0.0), humidity_results.get("chx_energy_used_kwh", 0.0)
-        )
+    total_power_used_kw = get_total_power_usage(
+        co2_results["co2_scrubber_power_used_kw"],
+        oga_results["oga_power_used_kw"],
+        light_results["light_power_used_kw"],
+        light_results["w_light_power_used_kw"],
+        thermal_outputs.get("radiator_power_kw", 0.0),
+        thermal_outputs.get("heater_power_kw", 0.0),
+        humidity_results.get("chx_power_used_kw", 0.0)
+    )
 
+    total_energy_used_kwh = get_total_energy_usage(
+    co2_results["co2_scrubber_energy_used_kwh"],
+    oga_results["oga_energy_used_kwh"],
+    light_results["light_energy_used_kwh"],
+    light_results["w_light_energy_used_kwh"],
+    thermal_outputs.get("radiator_energy_kwh", 0.0),
+    thermal_outputs.get("heater_energy_kwh", 0.0),
+    humidity_results.get("chx_energy_used_kwh", 0.0)
+    )
 
-    total_heat_added_kw, total_heat_added_kwh = total_heat_generated(
-        light_results["light_heat_kw"],
-        light_results["light_heat_kwh"],
-        wellness_results["w_light_heat_kw"],
-        wellness_results["w_light_heat_kwh"]
-        )   
-    
+    total_heat_added_kw = light_results["total_light_heat_kw"]
+    total_heat_added_kwh = light_results["total_light_heat_kwh"]
+
     new_battery_stored_kwh = max(0.0, battery_after_charge - total_energy_used_kwh)
 
     #------------dict for updating state-------------♡ 
@@ -266,6 +238,8 @@ def run_system_power(
         
         "total_heat_added_kw": total_heat_added_kw,
         "total_heat_added_kwh": total_heat_added_kwh,
+
+        **light_results
     }
 
     return power_updates, power_outputs
