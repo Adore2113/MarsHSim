7d60fa2f0237b11581b1dbf00017cb955aa980c3
Adore2113 <adore.codes@gmail.com>
2026-05-05 20:21:18 -0400

    run_buffer_gas_control function now returns two dicts, output and update in buffer_gas_system.py.

--- src/sim/buffer_gas_system.py
+++ src/sim/buffer_gas_system.py
@@ -128,16 +128,19 @@ def buffer_gas_power_and_heat(total_buffer_gas_added_kpa, total_buffer_gas_vente
 
 #-------buffer gas control info per timestep--------♡
 def run_buffer_gas_control(state, dt_min):
-    n2_kpa, ar_kpa, n2_stored_kg, ar_stored_kg, total_buffer_gas_added_kpa, total_buffer_gas_vented_kpa, buffer_gas_mode, pressure_gap_kpa = buffer_gas_control_kpa(state, dt_min)
-    
+    n2_kpa, ar_kpa, n2_stored_kg, ar_stored_kg, total_buffer_gas_added_kpa, total_buffer_gas_vented_kpa, buffer_gas_mode, pressure_gap_kpa = buffer_gas_control_kpa(state, dt_min) 
     buffer_gas_heat_added_kw, buffer_gas_heat_added_kwh, buffer_gas_power_used_kw, buffer_gas_energy_used_kwh = buffer_gas_power_and_heat(total_buffer_gas_added_kpa, total_buffer_gas_vented_kpa, dt_min)
     
-
-    return {
-        "n2_kpa": n2_kpa,
-        "ar_kpa": ar_kpa,
-        "n2_stored_kg": n2_stored_kg,
-        "ar_stored_kg": ar_stored_kg,
+    #------------dict for updating state-------------♡ 
+    buffer_gas_updates = {
+    "n2_kpa": n2_kpa,
+    "ar_kpa": ar_kpa,
+    "n2_stored_kg": n2_stored_kg,
+    "ar_stored_kg": ar_stored_kg,
+    }
+    
+    #-----------dict for printing outputs------------♡ 
+    buffer_gas_outputs = {
         "total_buffer_gas_added_kpa": total_buffer_gas_added_kpa,
         "buffer_gas_heat_added_kw": buffer_gas_heat_added_kw,
         "total_buffer_gas_vented_kpa": total_buffer_gas_vented_kpa,
@@ -146,5 +149,7 @@ def run_buffer_gas_control(state, dt_min):
         "buffer_gas_energy_used_kwh": buffer_gas_energy_used_kwh,
         "buffer_gas_mode": buffer_gas_mode, 
         "pressure_gap_kpa":  pressure_gap_kpa 
-    }
+    }   
+
+    return buffer_gas_updates, buffer_gas_outputs
 
