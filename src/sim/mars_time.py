2361bded9f1b3474211f05d2a09be7fbaa7fda63
Adore2113 <adore.codes@gmail.com>
2026-05-06 18:17:38 -0400

    cleaned up time logic, removed some constants and started adding some in for future systems in mars_time.py.

--- src/sim/mars_time.py
+++ src/sim/mars_time.py
@@ -4,23 +4,17 @@ import math
 
 
 #--------------------constants-----------------------♡
-seconds_per_sol = 88775     # one mars sol is 24h 39min 35sec
-hours_per_sol = seconds_per_sol / 3600
-sols_per_year = 668.6     # one full Mars orbit
+seconds_per_sol = 88775.244     # one mars sol is 24h 39min 35sec
+sols_per_year = 668.599     # one full Mars orbit
 
 # habitat location = 47° North, 184° East (Arcadia Planitia)
-longitude_east_deg = 184.0
 latitude_north_deg = 47.0
-
-# ls = areocentric solar longitude (season angle)
-solar_longitude_ls_deg_spring = 0    # equinox
-solar_longitude_ls_deg_summer = 90    # solstice
-solar_longitude_ls_deg_autumn = 180    # equinox
-solar_longitude_ls_deg_winter = 270    # solstice
+longitude_east_deg = 184.0
 
 mars_axial_tilt_deg = 25.19    # how much Mars is tilted for changing sun angle 
-
 max_daylight_m2_kw = 0.57
+low_sunlight_kw = 0.3    # < 0.3 sunlight per m2 = low sunlight
+min_growth_light_kw = 0.15    # 0.15 sunlight per m2 = min useful light ( FOR GREENHOUSE LATER )
 #----------------------------------------------------♡
 
 
@@ -39,44 +33,42 @@ def get_sol_time(state):
     sol_number = state.mission_time_s // seconds_per_sol
     sol_seconds = sol_time_seconds(state.mission_time_s)
 
-    sol_hour = sol_seconds // 3600
-    minutes = (sol_seconds % 3600) // 60
+    sol_hour = int(sol_seconds // 3600)
+    minutes = int((sol_seconds % 3600) // 60)
     
     return sol_number, sol_hour, minutes
 
 
 #----------------current season angle----------------♡ 
-def season_angle_deg(mission_time_s):    # ls = areocentric solar longitude (season angle)
+def get_season_angle_deg(mission_time_s):
     seconds_per_year = sols_per_year * seconds_per_sol
     degrees_per_second = 360.0 / seconds_per_year
 
-    current_season_angle_deg = (mission_time_s * degrees_per_second) % 360
+    season_angle_deg = (mission_time_s * degrees_per_second) % 360
     
-    return current_season_angle_deg
+    return season_angle_deg
 
 
 #-------how far the sun is shifted in the sky--------♡ 
-def solar_declination_deg(state):
-    current_season_angle_deg = season_angle_deg(state.mission_time_s)
-    solar_decline_deg = mars_axial_tilt_deg * math.sin(math.radians(current_season_angle_deg))
+def get_solar_decline_deg(state):
+    season_angle_deg = get_season_angle_deg(state.mission_time_s)
+    solar_decline_deg = mars_axial_tilt_deg * math.sin(math.radians(season_angle_deg))
 
     return solar_decline_deg
 
 
 #----------what fraction of sol is daylight----------♡ 
-def daylight_fraction_result(state):
-    latitude_north_rad = math.radians(latitude_north_deg)    #controls day length
-    solar_declination_rad = math.radians(solar_declination_deg(state))
+def get_daylight_fraction(state):
+    latitude_rad = math.radians(latitude_north_deg)    #controls day length
+    solar_declination_rad = math.radians(get_solar_decline_deg(state))
 
-    sun_visibility = -math.tan(latitude_north_rad) * math.tan(solar_declination_rad)    # how extreme the sun's tilt is vs how extreme the habitat position is
+    sun_visibility = -math.tan(latitude_rad) * math.tan(solar_declination_rad)    # how extreme the sun's tilt is vs how extreme the habitat position is
 
-    if sun_visibility <= -1:
-        daylight_fraction = 0.0
-        return daylight_fraction
+    if sun_visibility <= -1.0:
+        return 0.0
 
-    if sun_visibility >= 1:
-        daylight_fraction = 1.0
-        return daylight_fraction
+    if sun_visibility >= 1.0:
+        return 1.0
 
     sun_visibility_half_rad = math.acos(sun_visibility)
     daylight_fraction = sun_visibility_half_rad / math.pi
@@ -86,72 +78,69 @@ def daylight_fraction_result(state):
 
 #-----------when daylight starts and ends-----------♡
 def sunrise_sunset_seconds(state):
-    current_daylight_fraction = daylight_fraction_result(state)
+    current_daylight_fraction = get_daylight_fraction(state)
     
     daylight_seconds = int(seconds_per_sol * current_daylight_fraction)
     night_seconds = seconds_per_sol - daylight_seconds
-    half_night_seconds = night_seconds // 2
-    sunrise_seconds = half_night_seconds
+    sunrise_seconds = night_seconds // 2
     sunset_seconds = sunrise_seconds + daylight_seconds
+    
     return sunrise_seconds, sunset_seconds
 
 
 #-------------calculate sunlight amount-------------♡
-def determine_sunlight_amount(state):
+def get_sunlight_amount(state):
     current_sol_seconds = sol_time_seconds(state.mission_time_s)
     sunrise_seconds, sunset_seconds = sunrise_sunset_seconds(state)
 
     if current_sol_seconds < sunrise_seconds or current_sol_seconds > sunset_seconds:
-        sunlight_fraction = 0.0
-        return sunlight_fraction
-
-    daylight_seconds = sunset_seconds - sunrise_seconds
-    
-    if daylight_seconds == 0:
         sunlight_amount = 0.0
-        return sunlight_amount
     
-    seconds_since_sunrise = current_sol_seconds - sunrise_seconds
-    daylight_amount = seconds_since_sunrise / daylight_seconds
-
-    sunlight_amount = math.sin(math.pi * daylight_amount)
-    
+    else:
+        daylight_seconds = sunset_seconds - sunrise_seconds
+        
+        if daylight_seconds <= 0:
+            sunlight_amount = 0.0
+        
+        else:
+            seconds_since_sunrise = current_sol_seconds - sunrise_seconds
+            daylight_amount = seconds_since_sunrise / daylight_seconds
+            sunlight_amount = math.sin(math.pi * daylight_amount)
+            
     return sunlight_amount
 
 
+#---------------solar generation info----------------♡
+def get_daylight_per_m2_kw(state):
+    daylight_per_m2 = max_daylight_m2_kw * get_sunlight_amount(state)
+    
+    return daylight_per_m2
+
+
+#--------------define northern seasons---------------♡
+def current_mars_season(state):
+    season_angle_deg = get_season_angle_deg(state.mission_time_s)
+    # ls = areocentric solar longitude (season angle)
+
+    if 0 <= season_angle_deg < 90:
+        return "Northern Spring" 
+    
+    elif 90 <= season_angle_deg < 180:
+        return "Northern Summer" 
+    
+    elif 180 <= season_angle_deg < 270:
+        return "Northern Autumn" 
+    
+    else:
+        return "Northern Winter"
+    
+
 #--------------low sunlight streak info--------------♡
 def determine_low_sunlight_streak(state):
-    low_sunlight = 0.3
-
-    if state.peak_sunlight_today < low_sunlight:
+    if state.peak_sunlight_today < low_sunlight_kw:
         new_low_sunlight_streak_sols = state.low_sunlight_streak_sols + 1
     
     else:
         new_low_sunlight_streak_sols = 0
     
     return new_low_sunlight_streak_sols
-
-
-#---------------solar generation info----------------♡
-def daylight_per_m2_kw(state):
-    sunlight_amount = determine_sunlight_amount(state)
-    daylight_per_m2_kw = max_daylight_m2_kw * sunlight_amount
-    
-    return daylight_per_m2_kw
-
-
-#--------------define northern seasons---------------♡
-def current_mars_season(state):
-    current_season_angle_deg = season_angle_deg(state.mission_time_s)
-
-    if 0 <= current_season_angle_deg < 90:
-        return "north_spring" 
-    
-    elif 90 <= current_season_angle_deg < 180:
-        return "north_summer" 
-    
-    elif 180 <= current_season_angle_deg < 270:
-        return "north_autumn" 
-    
-    else:
-        return "north_winter"
