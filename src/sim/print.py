5e51cf0d2f51b1d0adc2db8070b8ed23e6d9e250
Adore2113 <adore.codes@gmail.com>
2026-05-28 17:38:13 -0400

    working on serperate file for printing.

--- src/sim/print.py
+++ src/sim/print.py
@@ -49,19 +49,11 @@ def print_environment(state, outputs):
     sol, hour, minutes = get_sol_time(state)
     temp_change_per_hour = outputs.get("temp_change_c", 0) * 12    # change this later
 
-
     print(f"{'Sol:':<19} {sol} | {hour:02d}:{minutes:02d} LMST")
-
     print(f"{'Habitat Temp:':<{lw}} {state.hab_temp_c:.3f} °C")
     print(f"{'Mars Temp:':<{lw}} {outputs['mars_temp_c']:.2f} °C")
 
-    print(f"{'Peak Sun Today:':<{lw}} {state.peak_sunlight_today:.3f} /1.0")
-    print(f"{'Low Sun Streak:':<{lw}} {state.low_sunlight_streak_sols} sols")
-
-    print(f"{'Sunlight per m²:':<{lw}} {state.daylight_m2_kw:.3f} kW")
 #----------------------------------------------------♡
-
-
 
 
 
@@ -77,16 +69,25 @@ def print_atmosphere(state, outputs):
     print(f"{'Nitrogen:':<{lw}} {state.n2_kpa:.2f} kPa")
     print(f"{'Argon:':<{lw}} {state.ar_kpa:.2f} kPa")
     
+    #------buffer gas control------♡
+    print(f"{'Buffer Gas Mode:':<{lw}} {outputs['buffer_gas_mode']}")
+    print(f"{'Pressure Gap:':<{lw}} {outputs['pressure_gap_kpa']:.3f} kPa")
+    
+    print(f"{'Gas Added:':<{lw}} {outputs['total_buffer_gas_added_kpa']:.3f} kPa")
+    print(f"{'Gas Vented:':<{lw}} {outputs.get('total_buffer_gas_vented_kpa', 0.0):.3f} kPa")
+
     #----------stored gas----------♡
     print(f"{'O2 Stored:':<{lw}} {state.o2_stored_kg:.2f} kg")
-    print(f"{'CO2 Stored:':<{lw}} {state.h2_stored_kg:.2f} kg")
-    print(f"{'N2 Stored:':<{lw}} {state.h2_stored_kg:.2f} kg")
-    print(f"{'AR Stored:':<{lw}} {state.h2_stored_kg:.2f} kg")
+    print(f"{'CO2 Stored:':<{lw}} {state.co2_stored_kg:.2f} kg")
+    print(f"{'N2 Stored:':<{lw}} {state.n2_stored_kg:.2f} kg")
+    print(f"{'AR Stored:':<{lw}} {state.ar_stored_kg:.2f} kg")
 
     print(f"{'H2 Stored:':<{lw}} {state.h2_stored_kg:.2f} kg")
     print(f"{'CH4 Stored:':<{lw}} {state.ch4_stored_kg:.2f} kg")    
     
     #-----------gas moved-----------♡
+    print(f"{'Amine Beds Online:':<22} {outputs.get('beds_online_count', 0)}")
+
     print(f"{'CO2 Consumed:':<{lw}} {outputs.get('sabatier_co2_consumed_kpa', 0):.3f} kPa")
     print(f"{'CO2 Scrubbed:':<{lw}} {outputs.get('co2_removed_kpa', 0):.3f} kPa")
 
@@ -97,36 +98,73 @@ def print_atmosphere(state, outputs):
     print(f"{'H2 Consumed:':<{lw}} {outputs.get('sabatier_h2_consumed_kg', 0):.2f} kg")
     print(f"{'O2 Produced:':<{lw}} {outputs.get('total_o2_produced_kpa', 0):.3f} kPa")
 
-    #------buffer gas control------♡
-    print(f"{'Buffer Gas Mode:':<{lw}} {outputs['buffer_gas_mode']}")
-    print(f"{'Pressure Gap:':<{lw}} {outputs['pressure_gap_kpa']:.3f} kPa")
-    
-    print(f"{'Gas Added:':<{lw}} {outputs['total_buffer_gas_added_kpa']:.3f} kPa")
-    print(f"{'Gas Vented:':<{lw}} {outputs.get('total_buffer_gas_vented_kpa', 0.0):.3f} kPa")
 
 #----------------------------------------------------♡
 
 
-
-#--------------------oxygen / oga--------------------♡
-def print_oga(state, outputs):
-    print_section_header("OXYGEN / OGA")
-
-#--------------------co2 scrubber--------------------♡
-def print_co2_scrub(state, outputs):
-    print_section_header("CO2 SCRUBBER")
-
-#--------------------buffer gas----------------------♡
-def print_environment(state, outputs):
-    print_section_header("BUFFER GAS")
-
 #-----------------------power------------------------♡
-def print_environment(state, outputs):
+def print_power(state, outputs):
     print_section_header("POWER")
 
+    #-------------solar-------------♡
+    print(f"{'Peak Sun Today:':<{lw}} {state.peak_sunlight_today:.3f} /1.0")
+    print(f"{'Sunlight per m²:':<{lw}} {state.daylight_m2_kw:.3f} kW")
+    print(f"{'Low Sun Streak:':<{lw}} {state.low_sunlight_streak_sols} sols")
+
+    print(f"\n{'Solar Arrays Online:':<{lw}} {outputs.get('solar_arrays_online_count', 0)} /10")
+    print(f"{'Solar Generated:':<{lw}} {outputs.get('total_solar_generated_kw', 0):.2f} kW")
+    #-------------stored------------♡
+    print(f"{'Battery Stored:':<{lw}} {state.battery_stored_kwh:.2f} kWh")
+    
+    #------------lights-------------♡
+    print(f"{'Wellness Lights:':<{lw}} {'ON' if state.wellness_lights_on else 'OFF'}")
+
+    #-----------power used----------♡
+    print(f"{'Total Power Used:':<{lw}} {outputs.get('total_power_used_kw', 0):.2f} kW")
+
+    print(f"{'GH Power Used:':<{lw}} {outputs.get('total_led_power_kw', 0):.2f} kW")
+    print(f"{'Power Used:':<{lw}} {outputs.get('sabatier_power_used_kw', 0):.2f} kW")
+    print(f"{'Scrubber Power Used:':<{lw}} {outputs.get('amine_bed_power_used_kw', 0):.2f} kW")
+    print(f"{'Lights Power:':<{lw}} {outputs.get('light_power_used_kw', 0):.2f} kW")
+    print(f"{'CHX Power Used:':<{lw}} {outputs.get('chx_power_used_kw', 0):.2f} kW")
+    print(f"{'Rad Power:':<{lw}} {outputs.get('radiator_power_kw', 0):.2f} kW\n")
+    print(f"{'Heater Power Used:':<{lw}} {outputs.get('heater_power_kw', 0):.2f} kW")
+
+    print(f"{'Total Energy Used:':<{lw}} {outputs.get('total_energy_used_kwh', 0):.2f} kWh")
+
+
 #----------------------thermal-----------------------♡
-def print_environment(state, outputs):
+def print_thermal(state, outputs):
     print_section_header("THERMAL")
+    print(f"{'Mars Temp:':<22} {outputs.get('mars_temp_c', state.mars_temp_c):.2f} °C")
+    print(f"{'Habitat Temp:':<22} {state.hab_temp_c:.2f} °C")
+
+    print(f"{'Temp Trend:':<20} ~ {temp_change_per_hour:.3f} °C/hr")    
+    
+    #-----------heat added----------♡
+    print(f"{'Net Heat:':<22} {outputs.get('net_heat_kw', 0):.2f} kW")
+    print(f"{'Heaters Online:':<22} {outputs.get('heaters_online_count', 0)}")
+    print(f"{'Heater Heat:':<22} {outputs.get('heater_heat_kw', 0):.2f} kW")
+
+
+    print(f"{'GH Heat Added:':<22} {outputs.get('total_greenhouse_heat_kw', 0):.3f} kW")
+
+    print(f"{'Heat Added:':<22} {outputs.get('sabatier_heat_added_kw', 0):.2f} kW")
+    print(f"{'Scrubber Heat:':<22} {outputs.get('amine_bed_heat_added_kw', 0):.2f} kW")
+
+    print(f"{'Lights Heat:':<22} {outputs.get('light_heat_kw', 0):.2f} kW")
+
+    print(f"{'Heat Loss:':<22} {outputs.get('heat_loss_kw', 0):.2f} kW")
+
+    print(f"{'CHX Heat:':<22} {outputs.get('chx_heat_added_kw', 0):.2f} kW")
+
+    #-----------cooling-------------♡
+    print(f"{'Radiators Online:':<22} {outputs.get('radiators_online_count', 0)}")
+    print(f"{'Rad Cooling:':<22} {outputs.get('radiator_heat_rejection_kw', 0):.2f} kW")
+        
+
+    #-----------power used----------♡
+
     print(f"{'OGA Heat:':<22} {outputs.get('oga_heat_kw', 0):.2f} kW")
 
 
@@ -168,5 +206,6 @@ def print_sim(state, outputs):
 
 
 
-       #  print(f"{'CO2 Stored:':<{lw}}" f"{state.co2_kpa:.3f} kPa + "f"{state.co2_stored_kg:.2f} kg stored")
+    #  print(f"{'CO2 Stored:':<{lw}}" f"{state.co2_kpa:.3f} kPa + "f"{state.co2_stored_kg:.2f} kg stored")
     #print(f"{'CO2 Consumed:':<{lw}} {outputs.get('total_co2_consumed_kpa', 0):.3f} kPa")
+    # print((f"\n♡  [THERMAL MODE: {outputs['hab_temp_mode']}]  ♡\n").center(width))
