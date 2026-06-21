34f31331bf320d93064d3732427a0b9f37c57acb
Adore2113 <adore.codes@gmail.com>
2026-06-20 20:15:51 -0400

    added compressor modes in isru_atm.py.

--- src/sim/isru_atm.py
+++ src/sim/isru_atm.py
@@ -16,3 +16,21 @@ mars_atm_ar_ratio = 0.016
 n2_low_storage_kg = 600.0
 ar_low_storage_kg = 300.0
 #----------------------------------------------------♡
+
+
+#----which compressors are online and how many-------♡
+def compressors_in_use(state, dt_min):
+    new_compressors = []
+    extracting_count = 0
+    deploying_count = 0
+    retracting_count = 0
+
+    for compressor in state.isru_compressors:
+        if compressor["status"] == "extracting":
+            extracting_count += 1
+ 
+        elif compressor["status"] == "deploying":
+            deploying_count += 1
+ 
+        elif compressor["status"] == "retracting":
+            retracting_count += 1
