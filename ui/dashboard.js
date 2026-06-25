e590525daa318e919d69f9d61edb58160cc68828
Adore2113 <adore.codes@gmail.com>
2026-06-25 19:55:17 -0400

    added to dashboard.js.

--- ui/dashboard.js
+++ ui/dashboard.js
@@ -2,8 +2,16 @@ async function loadDashboard() {
   const response = await fetch("data/latest.json");
   const data = await response.json();
 
-  document.getElementById("status").textContent =
-    `Status: ${data.system_status.status}`;
+const alerts = data.system_status.alerts.length > 0
+    ? data.system_status.alerts.join("<br>")
+    : "No alerts";
+
+  document.getElementById("status-box-p").innerHTML =
+    `Status: ${data.system_status.status}<br>` +
+    `Alerts: ${alerts}<br>` +
+    `Sol: ${data.environment.sol}<br>` +
+    `LMST: ${data.environment.lmst}<br>` +
+    `Mars Temp: ${data.environment.mars_temp_c.toFixed(1)} °C`;
 
   document.getElementById("time").textContent =
     `Sol ${data.environment.sol} | ${data.environment.lmst} LMST`;
