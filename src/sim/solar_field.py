84f0de6dca204260b7e6433c02521f27a665a664
Adore2113 <adore.codes@gmail.com>
2026-07-26 21:51:56 -0400

    added constants for area to solar_field.py.

--- src/sim/solar_field.py
+++ src/sim/solar_field.py
@@ -3,5 +3,7 @@ from .mars_time import get_sunlight_amount, current_mars_season
 #----------------------------------------------------♡
 
 #--------------------constants-----------------------♡
-
+land_area_acres = 50.0
+land_area_hectares = 20.23
+land_area_m2 = 202300.0
 #---------------------------------------------------♡
