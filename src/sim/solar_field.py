ff2c0b8caa78e6157acfa66797fb34740c829545
Adore2113 <adore.codes@gmail.com>
2026-08-02 19:26:10 -0400

    added to run_solar_field function in solar_fileds.py.

--- src/sim/solar_field.py
+++ src/sim/solar_field.py
@@ -8,12 +8,12 @@ land_area_hectares = 20.23
 land_area_m2 = 202300.0
 block_area_m2 = 4046.0
 
-total_panels = 101,250
+total_panels = 101250.0
 area_per_panel_m2 = 2.0
 
-total_arrays = 2,250
+total_arrays = 2250.0
 panels_per_array = 45
-area_per_panel_m2 = 89.9
+area_per_array_m2 = 89.9
 
 total_blocks = 50
 arrays_per_block = 45
@@ -32,10 +32,9 @@ summer_tilt_deg = 20.0  # placeholder
 winter_tilt_deg = 40.0  # placeholder
 
 #---panel performance---♡
-panel_efficiency = 0.20
-solar_conversion_ratio = 0.20
+solar_conversion_ratio = 0.20   # panel efficiency
 min_irradiance_w_per_m2 = 20.0
-clear_sy_peak_irradiance_w_per_m2 = 350.0   # clear sol average ~ 112 W/m2
+clear_sky_peak_irradiance_w_per_m2 = 350.0   # clear sol average ~ 112 W/m2
 
 #-----dust buildup------♡
 base_block_dust_rate_per_sol = 0.006    # open panels
@@ -114,7 +113,7 @@ def dust_and_cleaning(new_blocks, dt_min):
     for block in new_blocks:
         if block["flip_position"] == "up":
             dust_loss = base_block_dust_rate_per_sol * sols_per_step
-            block["dust_factor"] = max(min_irradiance_w_per_m2, block["dust_factor"] - dust_loss)
+            block["dust_factor"] = max(0.0, block["dust_factor"] - dust_loss)
 
             if block["dust_factor"] <= cleaning_trigger_dust_factor:
                 block["dust_factor"] = min(1.0, block["dust_factor"] + dust_factor_restored)
@@ -135,7 +134,7 @@ def get_block_generation(state, new_blocks, dt_min):
     hours_per_step = dt_min / 60.0
     sunlight_amount = get_sunlight_amount(state)
 
-    base_irradiance_w_per_m2 = clear_sy_peak_irradiance_w_per_m2 * sunlight_amount
+    base_irradiance_w_per_m2 = clear_sky_peak_irradiance_w_per_m2 * sunlight_amount
 
     if sunlight_amount > 0.0:
         irradiance_w_per_m2 = max(base_irradiance_w_per_m2, min_irradiance_w_per_m2)
@@ -147,7 +146,7 @@ def get_block_generation(state, new_blocks, dt_min):
 
     for block in new_blocks:
         if block["flip_position"] == "up":
-            block_power_kw = ((irradiance_w_per_m2 / 1000.0) * block_area_m2 * panel_efficiency * block["dust_factor"])
+            block_power_kw = ((irradiance_w_per_m2 / 1000.0) * block_area_m2 * solar_conversion_ratio * block["dust_factor"])
             total_field_power_generated_kw += block_power_kw
 
     total_field_energy_generated_kwh = total_field_power_generated_kw * hours_per_step
@@ -157,6 +156,27 @@ def get_block_generation(state, new_blocks, dt_min):
 
 #---------------------------------------------------♡
 def run_solar_field(state, dt_min):
-    new_blocks, blocks_up_count, flips_this_step, flip_energy_kwh, flip_power_kw = manage_block_flips(state, dt_min)
+    new_blocks, blocks_up_count, flips_this_step, flip_energy_used_kwh, flip_power_used_kw = manage_block_flips(state, dt_min)
     new_blocks, cleaned_this_step, cleaning_energy_used_kwh, cleaning_power_used_kw = dust_and_cleaning(new_blocks, dt_min)
     total_field_power_generated_kw, total_field_energy_generated_kwh = get_block_generation(state, new_blocks, dt_min)
+
+    solar_maintenance_power_used_kw = flip_power_used_kw + cleaning_power_used_kw
+    solar_maintenance_energy_used_kwh = flip_energy_used_kwh + cleaning_energy_used_kwh
+
+    #------------dict for updating state-------------♡ 
+    solar_field_updates = {"solar_blocks": new_blocks,}
+
+    #-----------dict for printing outputs------------♡ 
+    solar_field_outputs = {
+        "blocks_online_count": blocks_up_count,
+        "blocks_flipped_this_step": flips_this_step,
+        "blocks_cleaned_this_step": cleaned_this_step,
+
+        "solar_field_generated_kw": total_field_power_generated_kw,
+        "solar_field_generated_kwh": total_field_energy_generated_kwh,
+
+        "solar_field_power_used_kw": solar_maintenance_power_used_kw,
+        "solar_field_energy_used_kwh": solar_maintenance_energy_used_kwh,
+    }
+
+    return solar_field_updates, solar_field_outputs
