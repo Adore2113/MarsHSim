89c3c6acbe4c784f57fc52bd98b5d950108e04c0
Adore2113 <adore.codes@gmail.com>
2026-08-03 18:38:44 -0400

    moved lighting functions to it's own file from power.py.

--- src/sim/power.py
+++ src/sim/power.py
@@ -1,16 +1,11 @@
 #--------------------imports-------------------------♡
-from .mars_time import get_sol_time, get_sunlight_amount
+from .lights import light_system
 from .solar_field import run_solar_field
 #----------------------------------------------------♡
 
+# file for handling habitat power and batteries
+
 #--------------------constants-----------------------♡
-min_light_level = 0.2
-base_w_light_power_kw = 0.5
-base_w_light_heat_kw = 0.1
-
-base_light_power_kw = 2.0
-base_light_heat_kw = 0.5
-
 #---------backup bank----------♡
 max_transfer_speed_kw = 5000.0    # speed the power moves from bank - primary
 secondary_reserve_floor_pct = 0.10    # keep 10% if not in emergency
@@ -19,7 +14,6 @@ secondary_reserve_floor_pct = 0.10    # keep 10% if not in emergency
 primary_high_threshold_pct = 0.95    # start sending power to secondary
 primary_low_threshold_pct = 0.25    # start pulling from secondary
 primary_critical_pct = 0.20    # emergency, heavier pull from secondary
-
 #---------------------------------------------------♡
 
 
@@ -66,79 +60,6 @@ def manage_battery_bank(state, net_energy_kwh, dt_min):
     return new_primary_kwh, new_bank_kwh, bank_transfer_kwh
 
 
-#---------------------------------------------------♡
-#---------------------------------------------------♡
-
-
-#-----------habitat main light power info------------♡
-def light_system(state, dt_min, power_mode):
-    hours_per_step = dt_min / 60.0
-    _, sol_hour, minutes = get_sol_time(state)
-
-    sunlight_amount = get_sunlight_amount(state)
-    low_sunlight_streak = state.low_sunlight_streak_sols
-
-    #------------------main lights------------------♡ 
-    crew_awake_hours = 6 <= sol_hour < 21 or (sol_hour == 21 and minutes < 30)
-
-    if crew_awake_hours:
-        base_light_level = 1.0
-
-    else:
-        base_light_level = min_light_level
-
-    sunlight_dimming = sunlight_amount * 0.6    # sunlight level changes light level need for power saving
-    light_level_dimmed = base_light_level - sunlight_dimming
-    adjusted_light_level = max(min_light_level, light_level_dimmed)
-
-    #----------------wellness lights----------------♡ 
-    if low_sunlight_streak >= 3:
-        wellness_lights_on = True
-        wellness_light_level = 1.0
-    
-    elif low_sunlight_streak <= 1:
-        wellness_lights_on = False
-        wellness_light_level = 0.0
-    
-    else:
-        wellness_lights_on = state.wellness_lights_on
-        
-        if wellness_lights_on:
-            wellness_light_level = 1.0
-
-        else:
-            wellness_light_level = 0.0
-    
-    adjusted_light_level, wellness_light_level = apply_low_power_mode_lights(power_mode, adjusted_light_level, wellness_light_level)
-
-    light_power_used_kw = base_light_power_kw * adjusted_light_level
-    light_heat_kw =  base_light_heat_kw* adjusted_light_level
-
-    w_light_power_used_kw = base_w_light_power_kw * wellness_light_level
-    w_light_heat_kw =  base_w_light_heat_kw * wellness_light_level
-
-    #------------total light heat added-------------♡ 
-    total_light_heat_kw = light_heat_kw + w_light_heat_kw
-
-    return {
-        "adjusted_light_level": adjusted_light_level,
-        "light_heat_kw": light_heat_kw,
-        "light_heat_kwh": light_heat_kw * hours_per_step,
-        "light_power_used_kw": light_power_used_kw,
-        "light_energy_used_kwh": light_power_used_kw * hours_per_step,
-
-        "wellness_lights_on": wellness_lights_on,
-        "wellness_light_level": wellness_light_level,
-        "w_light_power_used_kw": w_light_power_used_kw,
-        "w_light_energy_used_kwh":  w_light_power_used_kw * hours_per_step,
-        "w_light_heat_kw": w_light_heat_kw,
-        "w_light_heat_kwh": w_light_heat_kw * hours_per_step,
-
-        "total_light_heat_kw": total_light_heat_kw,
-        "total_light_heat_kwh": total_light_heat_kw * hours_per_step,
-        }
-
-
 #------------------total power usage-----------------♡
 def get_total_power_usage(amine_bed_power_used_kw, oga_power_used_kw, light_power_used_kw, w_light_power_used_kw, greenhouse_led_power_kw, radiator_power_kw, heater_power_kw, chx_power_used_kw, upa_power_used_kw, wpa_power_used_kw, bpa_power_used_kw, sabatier_power_used_kw, isru_water_power_used_kw, isru_atm_power_used_kw, solar_field_power_used_kw):
     total_power_used_kw = (amine_bed_power_used_kw + oga_power_used_kw + light_power_used_kw + w_light_power_used_kw + greenhouse_led_power_kw + radiator_power_kw + heater_power_kw + chx_power_used_kw  + upa_power_used_kw + wpa_power_used_kw + bpa_power_used_kw + sabatier_power_used_kw + isru_water_power_used_kw + isru_atm_power_used_kw + solar_field_power_used_kw)
@@ -149,6 +70,7 @@ def get_total_energy_usage(amine_bed_energy_used_kwh, oga_energy_used_kwh, light
     total_energy_used_kwh = (amine_bed_energy_used_kwh + oga_energy_used_kwh + light_energy_used_kwh + w_light_energy_used_kwh + greenhouse_led_energy_kwh + radiator_energy_kwh + heater_energy_kwh + chx_energy_used_kwh + upa_energy_used_kwh + wpa_energy_used_kwh + bpa_energy_used_kwh + sabatier_energy_used_kwh + isru_water_energy_used_kwh + isru_atm_energy_used_kwh + solar_field_energy_used_kwh)  
 
     return total_energy_used_kwh
+
 
 #------------------full power system-----------------♡
 def run_system_power(
